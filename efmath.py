fb6c90f63a54256a41f719b42708ffe93d783afe
Elias <none@none>
2009-06-06 11:58:25 +0000

    Deleted combinatorics.py (mostly deprecated in Python 2.6); moved remaining functions to efmath.py.

--- efmath.py
+++ efmath.py
@@ -1,4 +1,7 @@
-# Note that gcd(a,b) * lcm(a,b) = a * b.
+"""Various mathematical utilities."""
+
+import math
+
 
 def gcd(x, y):
     """Calculate greatest common divisor."""
@@ -11,5 +14,22 @@ def gcd(x, y):
 
 
 def lcm(x, y):
-    """Calculate least common multiplier."""
+    """Calculate least common multiplier.
+
+    Uses the fact that gcd(x,y)*lcm(x,y)==x*y.
+    """
     return (x * y) / gcd(x, y)
+
+
+def countcombinations(n, m):
+    """Count combinations of m out of n items."""
+    if n < m:
+        raise ValueError
+    return math.factorial(n) / (math.factorial(m) * math.factorial(n - m))
+
+
+def countpermutations(n, m):
+    """Count permutaitons of m out of n items."""
+    if n < m:
+        raise ValueError
+    return math.factorial(n) / math.factorial(n - m)
