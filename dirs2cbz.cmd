99900e5df727a454d74babf11bbba257280959e0
Elias <none@none>
2009-07-30 15:10:10 +0000

    CBZ-related files cleanup.

--- dirs2cbz.cmd
+++
@@ -1,7 +0,0 @@
-@ECHO OFF & SETLOCAL ENABLEEXTENSIONS & hasHelp.py %* || GOTO skipHelp
-ECHO.Creates (uncompressed) CBZs from the files of all subdirs of current dir.
-ECHO.(C) Elias Fotinis
-GOTO :EOF
-:skipHelp
-
-FOR /D %d IN (*) DO @PUSHD "%d" & 7z a "..\%d" * -tzip -mx=0 & POPD
