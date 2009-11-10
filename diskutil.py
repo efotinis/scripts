84ed8aa13b9558bdbc92945a4b62370a6b120b73
Elias <none@none>
2009-11-10 16:18:57 +0000

    Renamed efDrives.py to diskutil.py.

---
+++ diskutil.py
@@ -0,0 +1,20 @@
+"""Disk management utilities."""
+
+import win32api
+import win32file
+
+
+def logical_drives(typefilter=None):
+    """String of available disk drives letters (e.g. 'ACD').
+
+    Result can be filtered by drive type (a single win32con.DRIVE_* value or a sequence of them).
+    """
+    bm = win32api.GetLogicalDrives()
+    letters = ''.join(chr(ord('A') + i) for i in range(26) if bm & (2 ** i))
+    if typefilter is not None:
+        try:
+            typefilter = set(typefilter)
+        except TypeError:
+            typefilter = set([typefilter])
+        letters = ''.join(c for c in letters if win32file.GetDriveType(c + ':\\') in typefilter)
+    return letters
