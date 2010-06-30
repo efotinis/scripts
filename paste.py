cddb9dbc026c60bbb567089ff00d8166a9e5e893
elias <none@none>
2010-06-30 14:56:22 +0000

    Added a help switch and proper EOL output to paste.py.

--- paste.py
+++ paste.py
@@ -1,15 +1,34 @@
 """Write clipboard text to STDOUT.
 
+usage: paste [-?]
+
 Will fail if there's no text in the clipboard.
 """
 
 import sys
+import getopt
 import win32console
 import clipboard
 
+try:
+    opts, args = getopt.gnu_getopt(sys.argv[1:], '?')
+    opts = dict(opts)
+    if '-?' in opts:
+        sys.stdout.write(__doc__)
+        sys.exit()
+    if args:
+        raise getopt.GetoptError('no parameters allowed')
+except getopt.GetoptError as x:
+    sys.stderr.write(str(x) + '\n')
+    sys.exit(2)
+
 text = clipboard.gettext()
 if text is None:
-    raise SystemExit('no text in clipboard')
+    sys.exit('no text in clipboard')
 
 encoding = 'cp' + str(win32console.GetConsoleOutputCP())
-sys.stdout.write(text.encode(encoding, 'replace'))
+text = text.encode(encoding, 'replace')
+# split lines manually to avoid CRLF->CRCRLF problems
+# TODO: can be optimized by splitting iteratively
+for s in text.splitlines():
+    print s
