cd582d72711ec269a49b7e34cdcad5b1a538dd09
elias <none@none>
2010-07-26 04:06:12 +0000

    Renamed fileattrchars() to win_attrib_str() and moved it from CommonTools.py to fsutil.py.

--- CommonTools.py
+++ CommonTools.py
@@ -11,7 +11,6 @@ import operator
 import win32console
 
 import wintime
-import binutil
 
 shell = shellcon = None  # delay load
 
@@ -202,31 +201,6 @@ def wintime_to_pyseconds(n):
 def pyseconds_to_wintime(n):
     """Convert Python seconds to a Windows FILETIME uint64."""
     return int(n / PY_TIME_SCALE + PY_EPOCH)
-
-
-def fileattrchars(n):
-    """String of chars from a DWORD of Windows file attributes.
-
-    Only documented/unreserved flags are returned:
-        0x00000001  FILE_ATTRIBUTE_READONLY             R
-        0x00000002  FILE_ATTRIBUTE_HIDDEN               H
-        0x00000004  FILE_ATTRIBUTE_SYSTEM               S
-        0x00000008  (undocumented)
-        0x00000010  FILE_ATTRIBUTE_DIRECTORY            D
-        0x00000020  FILE_ATTRIBUTE_ARCHIVE              A
-        0x00000040  FILE_ATTRIBUTE_DEVICE (reserved)
-        0x00000080  FILE_ATTRIBUTE_NORMAL               N
-        0x00000100  FILE_ATTRIBUTE_TEMPORARY            T
-        0x00000200  FILE_ATTRIBUTE_SPARSE_FILE          X
-        0x00000400  FILE_ATTRIBUTE_REPARSE_POINT        P
-        0x00000800  FILE_ATTRIBUTE_COMPRESSED           C
-        0x00001000  FILE_ATTRIBUTE_OFFLINE              O
-        0x00002000  FILE_ATTRIBUTE_NOT_CONTENT_INDEXED  I
-        0x00004000  FILE_ATTRIBUTE_ENCRYPTED            E
-        0x00008000  (undocumented)
-        0x00010000  FILE_ATTRIBUTE_VIRTUAL              V
-    """                                            
-    return binutil.flagchars(n, 32, 'RHS DA NTXPCOIE V')
 
 
 def nukeglobals(keep=None):
--- fsutil.py
+++ fsutil.py
@@ -2,6 +2,8 @@
 
 import contextlib
 
+import binutil
+
 
 @contextlib.contextmanager
 def preserve_cwd(path=None):
@@ -13,3 +15,31 @@ def preserve_cwd(path=None):
         yield
     finally:
         os.chdir(orgpath)
+
+
+def win_attrib_str(n):
+    """Windows DWORD attributes string.Formatter
+
+    Returns string with fixed len of 32, using spaces for missing/unknown flags.
+    Only documented/unreserved flags are returned:
+        0x00000001  FILE_ATTRIBUTE_READONLY             R
+        0x00000002  FILE_ATTRIBUTE_HIDDEN               H
+        0x00000004  FILE_ATTRIBUTE_SYSTEM               S
+        0x00000008  (undocumented)
+        0x00000010  FILE_ATTRIBUTE_DIRECTORY            D
+        0x00000020  FILE_ATTRIBUTE_ARCHIVE              A
+        0x00000040  FILE_ATTRIBUTE_DEVICE (reserved)
+        0x00000080  FILE_ATTRIBUTE_NORMAL               N
+        0x00000100  FILE_ATTRIBUTE_TEMPORARY            T
+        0x00000200  FILE_ATTRIBUTE_SPARSE_FILE          X
+        0x00000400  FILE_ATTRIBUTE_REPARSE_POINT        P
+        0x00000800  FILE_ATTRIBUTE_COMPRESSED           C
+        0x00001000  FILE_ATTRIBUTE_OFFLINE              O
+        0x00002000  FILE_ATTRIBUTE_NOT_CONTENT_INDEXED  I
+        0x00004000  FILE_ATTRIBUTE_ENCRYPTED            E
+        0x00008000  (undocumented)
+        0x00010000  FILE_ATTRIBUTE_VIRTUAL              V
+    """                                            
+    return binutil.flagchars(n, 32, 'RHS DA NTXPCOIE V')
+
+
