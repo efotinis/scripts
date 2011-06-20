f107bdf894e26dc2a1bd2a9c7ab3540759452d5a
elias <none@none>
2011-06-20 16:01:32 +0000

    Replaced custom cmdline parsing with argparse and refactored to proper module (script usage is still available).

--- locate.py
+++ locate.py
@@ -1,7 +1,9 @@
+"""Uses PATH and PATHEXT to locate Windows executables."""
+
 import os
 import sys
-
-import DosCmdLine
+import argparse
+import itertools
 
 
 def get_envlist(s):
@@ -13,81 +15,76 @@ def get_envlist(s):
 
 
 def errln(s):
+    """Write error line to STDERR."""
     sys.stderr.write('ERROR: ' + s + '\n')
 
 
-def strimatch(s1, s2):
-    return s1.lower() == s2.lower()
+def locate_file(name, exts, dirs, find_all):
+    """Search for file with extension in dirs and return list of found paths."""
+    ret = []
+    for (dir_, ext) in itertools.product(dirs, exts):
+        s = os.path.join(dir_, name + ext)
+        if os.path.isfile(s):
+            ret += [s]
+            if not find_all:
+                break
+    return ret
 
 
-def locate_file(fname, exts, paths, opt):
-    """Check a file and return whether it was located."""
-    found = False
-    if not opt.bareout:
-        print fname
-    for cur_path in paths:
-        for cur_ext in exts:
-            s = os.path.join(cur_path, fname + cur_ext)
-            if os.path.isfile(s):
-                if not opt.bareout:
-                    s = "  " + s
+def parse_args():
+    parser = argparse.ArgumentParser(description='locate executable files using the PATH and PATHEXT', add_help=False)
+    aa = parser.add_argument
+
+    aa('-a', dest='findall', action='store_true',
+       help='locate all occurences; by default, only the first one is shown')
+    aa('-b', dest='absolute', action='store_true',
+       help='output absolute paths')
+    aa('-v', dest='verbose', action='store_true',
+       help='verbose output; includes the input names')
+    aa('-?', action='help',
+       help='this help')
+    aa('files', metavar='FILE', nargs='+',
+       help='the file name to locate; directories and wildcards are not allowed')
+
+    args = parser.parse_args()
+    
+    for s in args.files:
+        if os.path.dirname(s):
+            parser.error('directory path not allowed: "%s"' % s)
+        if '?' in s or '*' in s:
+            parser.error('wildcards not allowed: "%s"' % s)
+            
+    return args
+
+
+if __name__ == '__main__':
+    args = parse_args()
+
+    # prepend '' to initially check without adding an extension
+    # TODO: maybe disable this?
+    path_exts = [''] + get_envlist('PATHEXT')
+
+    # on Windows the current directory is always checked first
+    path_dirs = ['.'] + get_envlist('PATH')
+    
+    # True when at least one on the files was not found
+    not_found = False
+    
+    for name in args.files:
+        a = locate_file(name, path_exts, path_dirs, args.findall)
+        if not a:
+            not_found = True
+            errln('file not found: "%s"' % name)
+            continue
+        if args.absolute:
+            a = [os.path.abspath(s) for s in a]
+        if args.verbose:
+            print name
+            for s in a:
+                print '  ' + s
+        else:
+            for s in a:
                 print s
-                found = True
-                if not opt.findall:
-                    return found
-    if not found and not opt.bareout:
-        print "  <not found>"
-    return found
-
-
-def locate(files, opt):
-    """Check a list of files and return true if at least one was located."""
-    # insert '' to initially check without adding an extension
-    exts = [''] + get_envlist('PATHEXT')
-    # insert the CWD to initially look in the current dir
-    paths = [os.getcwd()] + get_envlist('PATH')
-    found = False
-    for s in files:
-        if locate_file(s, exts, paths, opt):
-            found = True
-    return found
-
-
-def showHelp(switches):
-    print """\
-Locates executable files, using the PATH and PATHEXT environment variables.
-Elias Fotinis 2005-2007
-
-LOCATE.PY [/A] [/B] files
-"""
-    DosCmdLine.showlist(switches)
-    print """
-ERRORLEVEL is set to 0 if at least one instance of any of the specified files
-is found, or 1 otherwise. It's set to 2 if the parameters are wrong."""
-
-
-def main(args):
-    Flag = DosCmdLine.Flag
-    switches = [
-        Flag('?', 'help', None),
-        Flag('A', 'findall',
-             'Locate all occurences, instead of just the first one.'),
-        Flag('B', 'bareout',
-             'Print only found file paths. By default, the specified files '
-             'on the command line are printed, followed by the found path '
-             'names or a not-found message.'),
-        Flag('files', None,
-             'The files to locate. Directories and wildcards are not allowed.'),
-    ]
-    try:
-        opt, files = DosCmdLine.parse(args, switches)
-    except DosCmdLine.Error, x:
-        errln(str(x))
-        return 2
-    if opt.help:
-        showHelp(switches)
-        return 0
-    return 0 if locate(files, opt) else 1
-
-
-sys.exit(main(sys.argv[1:]))
+            
+    if not_found:
+        sys.exit(1)
