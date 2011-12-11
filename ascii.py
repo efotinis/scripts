3c06194f8352fc9fcc5d87aaeb217d8f99aea054
elias <none@none>
2011-12-11 13:59:22 +0000

    Replaced getopt with argparse.

--- ascii.py
+++ ascii.py
@@ -1,46 +1,35 @@
-"""Print ASCII chars in 32-255 range."""
+"""ASCII map printer."""
 
 import sys
-import getopt
-import CommonTools
+import argparse
 
 
-def help():
-    scriptname = CommonTools.scriptname().upper()
-    print """
-Print an ASCII map of the characters in the 32-255 range.
+def parse_args():
+    ap = argparse.ArgumentParser(
+        description='ASCII map printer',
+        add_help=False)
+    add = ap.add_argument
+    add('-n', dest='rowsize', action='store_const', const=16, default=32,
+        help='narrow ouput (%(const)d chars/line); default is %(default)d')
+    add('-b', dest='bareoutput', action='store_true',
+        help='bare output (no row/column labels)')
+    add('-?', action='help', help='this help')
+    return ap.parse_args()
 
-%(scriptname)s options
 
-  -n  Narrow map (16x14). Default is wide (32x7).
-  -b  Bare output: no top & side counters.
-"""[1:-1] % locals()
-    raise SystemExit
+if __name__ == '__main__':
+    args = parse_args()
 
+    # header
+    if not args.bareoutput:
+        headerfmt = '%01x' if args.rowsize <= 16 else '%02x'
+        headers = map(''.join, zip(*[headerfmt % i for i in range(args.rowsize)]))
+        for s in headers:
+            print '   ' + s
+        print '   ' + '-' * args.rowsize
 
-rowsize = 32
-bareoutput = False
-
-try:
-    opt, args = getopt.getopt(sys.argv[1:], '?hnb')
-    for sw, val in opt:
-        if sw == '-n':
-            rowsize = 16
-        elif sw == '-b':
-            bareoutput = True
-        elif sw == '-?' or sw == '-h':
-            help()
-except getopt.GetoptError as err:
-    raise SystemExit(err)
-
-if not bareoutput:
-    headerfmt = '%01x' if rowsize <= 16 else '%02x'
-    headers = map(''.join, zip(*[headerfmt % i for i in range(rowsize)]))
-    for s in headers:
-        print '   ' + s
-    print '   ' + '-' * rowsize
-
-for i in range(32, 256, rowsize):
-    if not bareoutput:
-        sys.stdout.write('%02x|' % i)
-    print ''.join(chr(c) for c in range(i, i+rowsize))
+    # rows
+    for i in range(32, 256, args.rowsize):
+        if not args.bareoutput:
+            sys.stdout.write('%02x|' % i)
+        print ''.join(chr(c) for c in range(i, i+args.rowsize))
