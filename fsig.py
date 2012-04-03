2e4cdc2915bf44cc448b9fb021f34b72c3a43e76
elias <none@none>
2012-04-03 10:00:33 +0000

    Improved names and code/UI separation, added comments, and fixed minor bugs.

--- fsig.py
+++ fsig.py
@@ -59,6 +59,7 @@ class Crc32:
     def hexdigest(self):
         return binascii.hexlify(self.digest())
     def copy(self):
+        # the only state of CRC32 is its current value
         ret = Crc32()
         ret.value = self.value
         return ret
@@ -78,9 +79,10 @@ def parse_args():
         add_help=False)
 
     add = parser.add_argument
+
     add('files', metavar='FILE', nargs='+',
         help='input file (glob pattern)')
-    add('-t', dest='hashFactory', choices=HASH_TYPES,
+    add('-t', dest='hash_type', choices=HASH_TYPES,
         default=DEFAULT_HASH_TYPE, metavar='TYPE',
         help='hash type; one of ' + ','.join(sorted(HASH_TYPES)) + '; '
              'default: %(default)s')
@@ -90,14 +92,14 @@ def parse_args():
          help='number of bytes to process (or -1 for all); default: %(default)s')
     add('-b', dest='buflen', type=size_param, default=DEFAULT_BUFFER_SIZE,
          help='read buffer size; default: %(default)s')
-    add('-u', dest='ucase', action='store_true',
-         help='output uppercase hex digits')
+    add('-u', dest='uppercase', action='store_true',
+         help='output hashes in uppercase')
     add('-i', dest='invert', action='store_true',
          help='invert output by displaying the file name first; '
               'useful for creating SFV files')
     add('-v', dest='verify', 
          help='list of comma-separated hashes to check against the specified files')
-    add('--np', dest='progress', action='store_false', default=True,
+    add('-P', dest='progress', action='store_false', default=True,
          help='do not display progress indicator; '
               'set automatically when STDOUT is not a console')
     add('-?', action='help', help='this help')
@@ -114,21 +116,23 @@ def parse_args():
     if args.buflen <= 0:
         parser.error('buffer size must be > 0')
 
-    if args.hashFactory == 'crc32':
-        args.hashFactory = Crc32
+    if args.hash_type == 'crc32':
+        args.hash_type = Crc32
     else:
-        args.hashFactory = lambda s=args.hashFactory: hashlib.new(s)
+        args.hash_type = lambda s=args.hash_type: hashlib.new(s)
 
     if args.verify:
-        strsize = args.hashFactory().digest_size * 2
+        strsize = args.hash_type().digest_size * 2
         a = []
         for s in args.verify.split(','):
+            s = s.strip().lower()
             if len(s) != strsize:
                 parser.error('invalid verify hash size: "%s"' % s)
             try:
-                a += [int(s, 16)]
+                int(s, 16)
             except ValueError:
-                parser.error('invalid verify hash: "%s"' % s)
+                parser.error('invalid verify hash value: "%s"' % s)
+            a += [s]
         args.verify = a
 
     args.files = [unicode(s, 'mbcs') for s in args.files]
@@ -150,104 +154,117 @@ def calc_read_size(fsize, offset, length):
 
 
 class ProgressIndicator(object):
-    def __init__(self, total):
+    """Temporary progress indicator with percent value and spinner."""
+    def __init__(self, name, total):
         self.current = 0
         self.total = total
         self.spinner = itertools.cycle('/-\\|')
-        print s, '... ',
+        # NOTE: if name is too long, SamePosOutput will only erase the last row
+        print name, '... ',
         self.spo = console_stuff.SamePosOutput()
     def update(self, n):
         self.current += n
         self.spo.restore()
-        percent = (self.current*100/self.total if self.total else 0)
-        print str(percent) + '%', self.spinner.next()
+        percent = (100.0 * self.current / self.total if self.total else 0)
+        print '%.0f%% %s' % (percent, self.spinner.next())
     def clear(self):
-        self.spo.pos.X = 0
+        self.spo.pos.X = 0  # include the name
         self.spo.restore(True)
+        sys.stdout.write('')  # softspace kludge
 
 
-def fileSig(s, opt, verify=None):
-    """Calculate and print file hash.
-
-    If 'verify' is set, tests and prints whether hash matches.
-    """
+def calc_hash(fp, offset, size, hash, buflen, progress):
+    """Calculate stream data digest. May raise IOError or EOFError."""
     try:
-        f = open(s, 'rb')
-        
-        if opt.progress:
-            total = calc_read_size(fileutil.fsize(f), opt.offset, opt.length)
-            progress = ProgressIndicator(total)
-        else:
-            progress = None
-            
-        f.seek(opt.offset)
-        hash = opt.hashFactory()
-        bytesToGo = opt.length
-        while bytesToGo != 0:
-            buf = f.read(opt.buflen)
-            if buf == '': break
-            if bytesToGo != -1 and len(buf) > bytesToGo:
-                buf = buf[:bytesToGo]
+        fp.seek(offset)
+        bytes_left = size
+        while bytes_left > 0:
+            buf = fp.read(min(buflen, bytes_left))
+            if buf == '':
+                raise EOFError('no enough data')
             hash.update(buf)
             if progress:
                 progress.update(len(buf))
-            if bytesToGo != -1:
-                bytesToGo = bytesToGo - len(buf)
+            bytes_left -= len(buf)
+    finally:
         if progress:
             progress.clear()
-        if bytesToGo > 0:
-            print 'Reached EOF while reading "%s"' % s
-        else:
-            digest = hash.hexdigest()
-            if opt.ucase:
-                digest = digest.upper()
-            # the empty write() prevents SamePosOutput from
-            # accidentally printing at col 1 (instead of 0)
-            sys.stdout.write('')
-
-            if verify:
-                digest = '   OK   ' if verify == int(digest, 16) else '**FAIL**'
-            
-            if opt.invert:
-                print s, digest
-            else:
-                print digest, s
-    except IOError as x:
-        fname = x.filename
-        if fname is None:
-            fname = s
-        CommonTools.errln(x.strerror + ': ' + s)
+    return hash.hexdigest()
 
 
-def files_gen(patterns, not_matched_handler=None):
-    """Generate file paths from list of file glob patterns."""
+class EmptyGlobHandler(object):
+    def __init__(self):
+        self.occured = False
+    def __call__(self, patt):
+        print >>sys.stderr, 'no matches for "%s"' % patt
+        self.occured = True
+        
+
+def files_gen(patterns, empty_handler=None):
+    """Generate file paths from a list of glob patterns.
+
+    If specified, empty_handler is called with a single argument,
+    which represents a pattern with no matches.
+    """
     for patt in patterns:
-        matched_something = False
+        emtpy = True
         for s in glob.glob(patt):
             if os.path.isfile(s):
                 yield s
-                matched_something = True
-        if not matched_something and not_matched_handler is not None:
-            not_matched_handler(patt)
+                emtpy = False
+        if emtpy and empty_handler:
+            empty_handler(patt)
 
-
-def not_matched_handler(patt):
-    print >>sys.stderr, 'WARNING: no matches for "%s"' % patt
-        
 
 if __name__ == '__main__':
     args = parse_args()
-##    print args
+
+
+    # FIXME: iscon() still returns True if running on console with stdout redirected.
+    # It only returns False on PythonWin.
+##    if console_stuff.iscon(sys.stdout.fileno()):
+##        import win32api
+##        win32api.MessageBeep()
+##    
 ##    sys.exit()
 
-    # TODO: notify user if a pattern has no matches
-    filepaths = list(files_gen(args.files, not_matched_handler))
+    empty_globs = EmptyGlobHandler()
+    filepaths = files_gen(args.files, empty_globs)
 
     if args.verify:
-        if len(args.verify) != len(filepaths):        
+        # check for length match early
+        filepaths = list(filepaths)
+        if len(filepaths) != len(args.verify):
             sys.exit('number of specified verify hashes does not match file count')
-        for s, sig in zip(filepaths, args.verify):
-            fileSig(s, args, verify=sig)
-    else:
-        for s in filepaths:
-            fileSig(s, args)
+
+    error_occured = False
+    expected_hashes = args.verify or itertools.repeat(None)
+
+    for (path, expected_hash) in itertools.izip(filepaths, expected_hashes):
+        try:
+            fp = open(path, 'rb')
+            hashobj = args.hash_type()
+            total = calc_read_size(fileutil.fsize(fp), args.offset, args.length)
+            progress = ProgressIndicator(path, total) if args.progress else None
+            hash = calc_hash(fp, args.offset, total, hashobj, args.buflen, progress)
+            if not expected_hash:
+                if args.uppercase:
+                    hash = hash.upper()
+                if args.invert:
+                    print path, hash
+                else:
+                    print hash, path
+            else:
+                if hash != expected_hash:
+                    print 'FAIL ', path
+                    print >>sys.stderr, 'hash mismatch for "%s": expected %s, got %s' % (path, expected_hash, hash)
+                    error_occured = True
+                else:
+                    print 'OK   ', path
+        except (IOError, EOFError) as x:
+            print >>sys.stderr, 'error while hashing "%s": %s' % (path, x)
+
+    if empty_globs.occured:
+        error_occured = True
+
+    sys.exit(1 if error_occured else 0)
