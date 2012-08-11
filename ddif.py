93c1d65d056756ee3b5b4cbfb5c20e28a42a0936
elias <none@none>
2012-08-11 14:44:25 +0000

    Added case-sensitivity option and byte-by-byte contents comparison. Refactored output code out of comparison functions.

--- ddif.py
+++ ddif.py
@@ -1,4 +1,4 @@
-"""Compare the files (and optionally their hashes) of two directories."""
+"""Compare the files (and optionally their hashes/bytes) of two directories."""
 
 import os
 import sys
@@ -7,65 +7,96 @@ import argparse
 import CommonTools
 
 
-def merged(a, b):
-    """Merge two sorted lists.
+IO_BUFLEN = 2 ** 20
+
+
+if os.name == 'posix':
+    CASE_SENSITIVE_PLATFORM_PATHS = True
+elif os.name == 'nt':
+    CASE_SENSITIVE_PLATFORM_PATHS = False
+else:
+    CASE_SENSITIVE_PLATFORM_PATHS = None
+
+
+def merged(a, b, case_sens):
+    """Merge two lists.
 
     Generates each item and a character. The character shows in which list
     the item was found: '<' (first), '>' (second), or '=' (both).
     """
-    a, b = a[:], b[:]
+    if case_sens:
+        sort_key = lambda s: s
+        item_cmp = lambda s1, s2: cmp(s1, s2)
+    else:
+        sort_key = lambda s: s.lower()
+        item_cmp = lambda s1, s2: cmp(s1.lower(), s2.lower())
+
+    a = sorted(a, key=sort_key)
+    b = sorted(b, key=sort_key)
+
     while a or b:
         if not a:
             yield b.pop(0), '>'
         elif not b:
             yield a.pop(0), '<'
         else:
-            if a[0] == b[0]:
+            order = item_cmp(a[0], b[0])
+            if order < 0:
+                yield a.pop(0), '<'
+            elif order > 0:
+                yield b.pop(0), '>'
+            else:
                 b.pop(0)
                 yield a.pop(0), '='
-            elif a[0] < b[0]:
-                yield a.pop(0), '<'
-            else:
-                yield b.pop(0), '>'
 
 
-def fsig(fn, buflen=2**20):
-    hash = hashlib.md5()
-    with open(fn, 'rb') as f:
+def compare_file_hashes(path1, path2):
+    if os.path.getsize(path1) != os.path.getsize(path2):
+        return False
+    hash1, hash2 = hashlib.md5(), hashlib.md5()
+    with open(path1, 'rb') as f1, open(path2, 'rb') as f2:
         while True:
-            s = f.read(buflen)
-            if not s:
-                break
-            hash.update(s)
-    return hash.digest()
+            s1, s2 = f1.read(IO_BUFLEN), f2.read(IO_BUFLEN)
+            if len(s1) != len(s2):
+                # size mismatch
+                return False
+            elif not s1:
+                # both files exhausted
+                return True
+            else:
+                hash1.update(s1)
+                hash2.update(s2)
+                if hash1.digest() != hash2.digest():
+                    return False
 
 
-def compare_files(root1, root2, rel, name, stats):
-    # TODO: replace args.testmethod with an actual function
-    if args.testmethod == 'n':
-        pass
-    elif args.testmethod == 'h':
-        fn1 = os.path.join(root1, rel, name)
-        fn2 = os.path.join(root2, rel, name)
-        # TODO: checks hashes (or even bytes) in parallel
-        if fsig(fn1) != fsig(fn2):
-            print '**', os.path.join(rel, name)
-            stats['mismatched_files'] += 1
-            return
-    elif args.testmethod == 'b':
-        raise NotImplementedError('byte compare')
-    else:
-        assert False, 'invalid test method: "%s"' % args.testmethod
+def compare_file_bytes(path1, path2):
+    if os.path.getsize(path1) != os.path.getsize(path2):
+        return False
+    with open(path1, 'rb') as f1, open(path2, 'rb') as f2:
+        while True:
+            s1, s2 = f1.read(IO_BUFLEN), f2.read(IO_BUFLEN)
+            if len(s1) != len(s2):
+                # size mismatch
+                return False
+            elif not s1:
+                # both files exhausted
+                return True
+            else:
+                if s1 != s2:
+                    return False
         
-    if args.verbose:
-        print '==', os.path.join(rel, name)
-    stats['matched_files'] += 1
-        
 
-def compare_dirs(root1, root2, rel, stats):
-    a1 = sorted(os.path.normcase(s) for s in os.listdir(os.path.join(root1, rel)))
-    a2 = sorted(os.path.normcase(s) for s in os.listdir(os.path.join(root2, rel)))
-    for name, where in merged(a1, a2):
+def compare_dirs(root1, root2, rel, stats, args):
+    """Compare two dirs."""
+    # FIXME: also list all files from unmatched directories
+
+    merged_names = merged(
+        os.listdir(os.path.join(root1, rel)),
+        os.listdir(os.path.join(root2, rel)),
+        args.case_sens)
+
+    for name, where in merged_names:
         if where in '<':
             print '1 ', os.path.join(rel, name)
             stats['only_in_a'] += 1
@@ -76,9 +107,17 @@ def compare_dirs(root1, root2, rel, stats):
             isdir1 = os.path.isdir(os.path.join(root1, rel, name))
             isdir2 = os.path.isdir(os.path.join(root2, rel, name))
             if isdir1 and isdir2:
-                compare_dirs(root1, root2, os.path.join(rel, name), stats)
+                compare_dirs(root1, root2, os.path.join(rel, name), stats, args)
             elif not isdir1 and not isdir2:
-                compare_files(root1, root2, rel, name, stats)
+                path1 = os.path.join(root1, rel, name)
+                path2 = os.path.join(root2, rel, name)
+                if args.content_matcher(path1, path2):
+                    if args.verbose:
+                        print '==', os.path.join(rel, name)
+                    stats['matched_files'] += 1
+                else:
+                    print '**', os.path.join(rel, name)
+                    stats['mismatched_files'] += 1
             else:
                 print '!!', os.path.join(rel, name)
                 stats['file_dir_mismatches'] += 1
@@ -93,9 +132,10 @@ def parse_args():
     add('dir1', metavar='DIR1', help='first directory')
     add('dir2', metavar='DIR2', help='second directory')
     add('relpath', metavar='REL', nargs='?', help='common relative path')
-    #add('-c', dest='matchcase', action='store_true', help='force case-sensitive comparisons')
-    add('-t', dest='testmethod', choices='nhb', default='n',
-        help='test method for matching files; n: by name (default), h: by MD5 hash, b: by bytes')
+    add('--case-sens', choices='mip', default='p',
+        help='name case comparisons; m: match, i: ignore, p: platform specific (default)')
+    add('-c', dest='content_matcher', choices='nhb', default='n',
+        help='file contents comparison; n: none (default), h: MD5 hash, b: bytes')
     #add('-b', dest='buflen', type=int, default=2*20, help='file I/O buffer size')
     add('-v', dest='verbose', action='store_true', help='verbose output; shows matched items')
     add('-?', action='help', help='this help')
@@ -113,13 +153,30 @@ def parse_args():
 
     args.dir1 = unicode(args.dir1)
     args.dir2 = unicode(args.dir2)
-    
+
+    if args.case_sens == 'm':
+        args.case_sens = True
+    elif args.case_sens == 'i':
+        args.case_sens = False
+    else:
+        if CASE_SENSITIVE_PLATFORM_PATHS is None:
+            parser.error('current platforn path case sensitivity is unknown')
+        args.case_sens = CASE_SENSITIVE_PLATFORM_PATHS
+
+    args.content_matcher = {
+        'n': lambda s1, s2: True,
+        'h': compare_file_hashes,
+        'b': compare_file_bytes,
+    } [args.content_matcher]
+
     return args
 
 
 if __name__ == '__main__':
 
     args = parse_args()
+##    print args
+##    sys.exit()
 
     dir1, dir2 = args.dir1, args.dir2
     del args.dir1, args.dir2
@@ -127,7 +184,7 @@ if __name__ == '__main__':
     stats = {'only_in_a':0, 'only_in_b':0, 'file_dir_mismatches':0,
              'mismatched_files':0, 'matched_files':0}
     try:
-        compare_dirs(dir1, dir2, '', stats)
+        compare_dirs(dir1, dir2, '', stats, args)
         print
         print '            only in 1:', stats['only_in_a']
         print '            only in 2:', stats['only_in_b']
