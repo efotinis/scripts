dd62efaecfa7f5d2bf67b496af46d3e4804f2016
elias <none@none>
2013-06-27 10:30:22 +0000

    Improved output path formating.

--- imgsplit.py
+++ imgsplit.py
@@ -6,10 +6,20 @@ import argparse
 from PIL import Image
 
 
+PATH_FORMAT_VARS = [
+    ('name', 'input name'),
+    ('ext', 'input ext'),
+    ('row', 'grid row'),
+    ('col', 'grid col'),
+    ('i', 'row-major index'),
+    ('j', 'column-major index'),
+]
+
+
 def parse_args():
+    DEFAULT_OUTPATH = '{name}{i}{ext}'
     parser = argparse.ArgumentParser(
-        description='split an image to a grid of subimages',
-        add_help=False)
+        description='split an image to a grid of subimages')
 
     _add = parser.add_argument
 
@@ -19,21 +29,20 @@ def parse_args():
          help='number of rows (>0)')
     _add(dest='cols', metavar='COLS', type=int,
          help='number of columns (>0)')
-    _add('-o', dest='outfile', metavar='DST',
-         help='output file; available {}-style formatting vars: '
-              '"r" (row), "c" (column), "i" (row-major index), '
-              '"j" (column-major index); default is "input{i}.ext"')
+    _add('-o', dest='outfile', metavar='DST', default=DEFAULT_OUTPATH, 
+         help='output file; can include directory path; '
+              'available {}-style formatting vars: ' +
+               ', '.join('"%s" (%s)' % (name, descr) for (name, descr) in PATH_FORMAT_VARS) + 
+               '; default is "%(default)s"')
     _add('-f', dest='format', metavar='FMT',
          help='output image format; default depends on output extension')
     _add('--overwrite', dest='overwrite', action='store_true', 
          help='overwrite existing output files; by default an error msg is printed')
-    _add('-?', action='help', help='this help')
 
     args = parser.parse_args()
 
     if args.outfile is None:
-        stem, ext = os.path.splitext(args.infile)
-        args.outfile = stem + '{i}' + ext
+        args.outfile = DEFAULT_OUTPATH
 
     if args.rows <= 0 or args.cols <= 0:
         parser.error('rows/columns must be >0')
@@ -46,6 +55,9 @@ if __name__ == '__main__':
 
     im = Image.open(args.infile)
     width, height = im.size
+
+    name, ext = os.path.splitext(os.path.basename(args.infile))
+    fmt_vars = {'name': name, 'ext': ext}
 
     errors = 0
     for row in range(args.rows):
@@ -60,10 +72,13 @@ if __name__ == '__main__':
             imnew.paste(im.copy().crop((x1, y1, x2, y2)))
 
             # format name and save output
-            i = row * args.cols + col
-            j = col * args.rows + row
-            outpath = args.outfile.format(r=row, c=col, i=i, j=j)
-            if not args.overwrite and os.path.exists(outpath):
+            fmt_vars['row'] = row
+            fmt_vars['col'] = col
+            fmt_vars['i'] = row * args.cols + col
+            fmt_vars['j'] = col * args.rows + row
+            outpath = args.outfile.format(**fmt_vars)
+
+            if os.path.exists(outpath) and not args.overwrite:
                 print >>sys.stderr, 'file already exists: "%s"' % outpath
                 errors += 1
             else:
