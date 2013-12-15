7777358ec231a92155d8a9466c3b66c660dff9de
elias <none@none>
2013-12-15 15:08:19 +0000

    Updated age-check URL format. Changed cmdline arguments to more usual defaults. Keep track of previously returned subreddits, to avoid duplicates.

--- randit.py
+++ randit.py
@@ -1,16 +1,23 @@
-"""Get random subreddits."""
+"""Go to random subreddits."""
+
+import os
+import re
 import sys
 import time
 import urllib2
+import urlparse
 import webbrowser
 import argparse
 import webutil
+import contextlib
 
 
 USER_AGENT = 'randit.py'
 REDDIT_REQUEST_DELAY_SEC = 2
 SFW_URL = 'http://random.reddit.com/'
 NSFW_URL = 'http://randnsfw.reddit.com/'
+DEFAULT_HISTORY_PATH = '%AppData%\\randit-seen.log'
+NAME_RX = re.compile(r'^http://www.reddit.com/r/(.*)/$')
 
 
 def get_destination(url):
@@ -25,45 +32,126 @@ def strip_age_check(url):
     """Remove the age verification redirect.
 
     Example:
-        'http://www.reddit.com/r/foobar/over18?dest=%2Fr%2Ffoobar%2F'
+        'http://www.reddit.com/over18?dest=http%3A%2F%2Fwww.reddit.com%2Fr%2Ffoobar%2F'
     becomes:
         'http://www.reddit.com/r/foobar/'
     """
-    return url.partition('over18?')[0]
+    s = urlparse.urlsplit(url).query
+    return urlparse.parse_qs(s)['dest'][0]
 
 
 def parse_args():
-    ap = argparse.ArgumentParser(description='get random subreddits')
+    ap = argparse.ArgumentParser(
+        description='visit random subreddits',
+        epilog='previously returned subreddit names are kept in "%s"' %
+            DEFAULT_HISTORY_PATH
+    )
     add = ap.add_argument
-    add('-n', dest='count', type=int, default=10,
+    add('-n', dest='count', type=int, default=5,
         help='number of subreddits (default: %(default)s)')
     add('-x', dest='nsfw', action='store_true', help='get NSFW subreddits')
-    add('-l', dest='launch', action='store_true',
-        help='launch URL in browser instead of printing')
-    add('-b', dest='batch', action='store_true',
-        help='print or launch everything at once (batch)')
+    add('-p', dest='print_', action='store_true',
+        help='print URLs instead of launching in browser')
+    add('-i', dest='incremental', action='store_true',
+        help='produce results incrementally; by default everything is output '
+        'at once')
+    add('-H', dest='nohistory', action='store_true',
+        help='ignore previously returned subreddits; this will prevent '
+        'nondeterministic delays caused by retrying')
     args = ap.parse_args()
     if args.count < 0:
         args.count = 0
     return args
 
 
-def url_generator(url, count):
-    """Generate subreddit URLs from a randomizer URL."""
-    for i in range(count):
+def get_name(url):
+    """Extract the subreddit name from its URL."""
+    return NAME_RX.match(url).group(1)
+
+
+def url_generator(nsfw, count, history):
+    """Generate subreddit URLs.
+
+    history is a set-like object that keeps track of already generated subs.
+    """
+    url = NSFW_URL if nsfw else SFW_URL
+    while count > 0:
         time.sleep(REDDIT_REQUEST_DELAY_SEC)
         # the additional delay caused by resolving the redirection prevents duplicates
-        yield strip_age_check(get_destination(url))
+        s = get_destination(url)
+        if nsfw:
+            s = strip_age_check(s)
+        name = get_name(s)
+        if name in history:
+            print >>sys.stderr, 'skipping already seen:', name
+            continue
+        yield s
+        history.add(name)
+        count -= 1
+
+
+class History(object):
+    """Keep track of a set of strings.
+
+    Use in a with statement to persist data in a simple text file.
+    """
+
+    def __init__(self, path):
+        self.path = path
+        self.data = None
+
+    def __enter__(self):
+        self.data = set()
+        try:
+            with open(self.path) as f:
+                self.data.update(s.rstrip('\n') for s in f)
+        except IOError:
+            pass
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        with open(self.path, 'w') as f:
+            for s in self.data:
+                print >>f, s
+        self.data = None
+
+    def add(self, s):
+        self.data.add(s)
+
+    def __contains__(self, s):
+        return s in self.data
+
+
+class DummyHistory(object):
+    """Dummy version of History class.
+
+    Always appears empty and doesn't store anything.
+    """
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        pass
+
+    def add(self, s):
+        pass
+
+    def __contains__(self, s):
+        return False
 
 
 if __name__ == '__main__':
     args = parse_args()
-    OPEN_NEW_TAB = 2
-    urls = url_generator(NSFW_URL if args.nsfw else SFW_URL, args.count)
-    if args.batch:
-        urls = list(urls)
-    for url in urls:
-        if args.launch:
-            webbrowser.open(url, new=OPEN_NEW_TAB, autoraise=False)
-        else:
-            print url
+    history = DummyHistory() if args.nohistory else \
+              History(os.path.expandvars(DEFAULT_HISTORY_PATH))
+    with history:
+        OPEN_NEW_TAB = 2
+        urls = url_generator(args.nsfw, args.count, history)
+        if not args.incremental:
+            urls = list(urls)
+        for url in urls:
+            if args.print_:
+                print url
+            else:
+                webbrowser.open(url, new=OPEN_NEW_TAB, autoraise=False)
