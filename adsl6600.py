df003d93a547e2a5b0fc4f1f24ec408c2b2e2348
elias <none@none>
2014-02-14 12:39:12 +0000

    Added interactive password input if not specified in cmdline. Return 'n/a' values instead of an exception when link is down.

--- adsl6600.py
+++ adsl6600.py
@@ -11,18 +11,25 @@ Currently prints one line showing:
 import os
 import sys
 import time
+import getpass
 import wbr6600
 
 
 if __name__ == '__main__':
     router_ip, user, token = sys.argv[1:]
-    token = str(token.decode('base64').decode('rot13'))
+    if token:
+        token = str(token.decode('base64').decode('rot13'))
+    else:
+        token = getpass.getpass()
 
     with wbr6600.Telnet(router_ip, user, token) as t:
         timestamp = time.ctime()
         ip = t.getWanProtoStatus()['IP address']
         chan = t.getWanAdslChandataSummary()
-        opmode = t.getWanAdslOpmode()['operational mode']
-        noise = t.getNoiseSummary()
+        opmode = t.getWanAdslOpmode().get('operational mode', 'n/a')
+        try:
+            noise = t.getNoiseSummary()
+        except KeyError:
+            noise = 'n/a'
 
     print '%s - %s, %s, %s, %s' % (timestamp, ip, chan, opmode, noise)
