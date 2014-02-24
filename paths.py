dd8ab0ad0aa3f1d316a243802ea12723f1ce6c01
elias <none@none>
2014-02-24 18:57:41 +0000

    Added existence and duplication tests in default invocation.

--- paths.py
+++ paths.py
@@ -2,20 +2,31 @@
 
 import os
 import sys
-import getopt
+import argparse
+import collections
 
 
-try:
-    opt, args = getopt.gnu_getopt(sys.argv[1:], '?')
-    opt = dict(opt)
-    if args:
-        raise getopt.GetoptError('no params needed')
-except getopt.GetoptError as err:
-    raise SystemExit('ERROR: ' + str(err))
+def parse_args():
+    ap = argparse.ArgumentParser(
+        description='print PATH directories',
+        epilog='flags shown: "*" non-existing, "!" douplicate')
+    add = ap.add_argument
+    add('-b', dest='bare', action='store_true',
+        help='print only paths')
+    return ap.parse_args()
 
-if '-?' in opt:
-    print 'Display a list of the PATH entries.'
-    raise SystemExit
 
-for s in os.environ['PATH'].split(os.path.pathsep):
-    print s
+if __name__ == '__main__':
+    args = parse_args()
+
+    items = os.environ['PATH'].split(os.path.pathsep)
+    dup_counts = collections.Counter(os.path.normcase(s) for s in items)
+    if args.bare:
+        for s in items:
+            print s
+    else:
+        for s in items:
+            flags = ''
+            flags += '*' if not os.path.isdir(s) else ' '
+            flags += '!' if dup_counts[os.path.normcase(s)] > 1 else ' '
+            print flags, s
