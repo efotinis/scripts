b4b973fc61c4e1c57efbdd0316f4dd62a5343774
Elias <none@none>
2015-06-14 14:04:23 +0000

    Added WAN IP logging script.
    The script first telnets the router and falls back to a WAN IP web service if needed.

---
+++ waniplog.py
@@ -0,0 +1,44 @@
+#!python3
+"""Update file containing WAN IP.
+
+Can be used to update a Dropbox file with my home IP.
+"""
+
+import sys
+
+import myip
+import tdw8970
+
+
+def get_ip(psw):
+    """Get current external WAN IP.
+
+    Try the router's telnet interface and fallback to a web service.
+    """
+    try:
+        psw = psw.encode('ascii')
+        with tdw8970.Telnet(password=psw, timeout=5) as t:
+            for d in t.wan_services():
+                if d['Proto'] == 'PPPoE':
+                    return d['IP']
+        print('IP not found using telnet', file=sys.stderr)
+    except Exception as x:
+        print('telnet session failed:', x, file=sys.stderr)
+    return myip.query()
+
+
+def update(path, ip):
+    """Write WAN IP to file if it's different."""
+    try:
+        with open(path) as f:
+            previous = f.read()
+    except OSError:
+        previous = ''
+    if ip != previous:
+        with open(path, 'w') as f:
+            f.write(ip)
+
+
+if __name__ == '__main__':
+    psw, path = sys.argv[1:]
+    update(path, get_ip(psw))
