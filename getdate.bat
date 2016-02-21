fdca5729150cbdb999bbe53f21ca917175531b8e
elias <none@none>
2016-02-21 15:01:01 +0000

    Removed old scripts.

--- getdate.bat
+++
@@ -1,4 +0,0 @@
-@ECHO OFF & SETLOCAL ENABLEEXTENSIONS
-:: GETDATE.EXE wrapper.
-:: (C) Elias Fotinis
-F:\Projects\console\getdate\Release\getdate.exe %*
--- getmem.bat
+++
@@ -1,4 +0,0 @@
-@ECHO OFF & SETLOCAL ENABLEEXTENSIONS
-:: GETMEM.EXE wrapper.
-:: (C) Elias Fotinis
-"D:\projects\system\getmem\Release\getmem.exe" %*
