278797fed36903962318fad0b0b2302e84e4221a
elias <none@none>
2016-02-21 19:15:50 +0000

    Complete overhaul.
    - Converted to Python 3 and removed workarounds for old bugs.
    - Removed locale and codepage functionality (the first was used for digit grouping and is now covered by str.format; the second should be handled automatically by efutil.conout and the console codepage).
    - Simplified directory scanning and added error reporting for os.walk.
    - Simplified input pattern handling.

--- deepDir.py
+++ deepDir.py
@@ -1,137 +1,31 @@
-"""Display file/subdir/byte counts of directories."""
+#!python3
+"""Display recursive file/dir/byte counts of directories."""
 
-import os
-import sys
-import locale
-import re
 import argparse
+import os
+import re
 
 import efutil
-import winfixargv
-
-
-# TODO: cleanup needed (esp. old Python bugs)
-# FIXME: '.' doesn't display cur dir
-# FIXME: Unicode output
-
-
-RAWPATH_PREFIX = '\\\\?\\'  # disable path parsing
-
-
-def getsize(path):
-    """Get the size of a file, even if len(path) > MAX_PATH."""
-    try:
-        return os.path.getsize(path)
-    except WindowsError:
-        # probably ERROR_PATH_NOT_FOUND
-        return os.path.getsize(RAWPATH_PREFIX + path)
-
-
-def deepDir(root):
-    """Returns the total dirs, files and bytes in the specified dir."""
-    #assert type(root) == unicode
-    dirsCnt = filesCnt = bytes = 0
-    # Use abspath() to remove '.' and '..'. Otherwise, join() may result
-    # in paths longer than MAX_PATH and stat() in getsize() will fail.
-    # normpath() isn't sufficient, because it can't remove leading '.' or '..'.
-    # Note that using the absolute path in os.walk() is a bit slower,
-    # but the loop runs faster because we don't have to normpath()
-    # when joining paths with filenames!
-    for path, dirs, files in os.walk(os.path.abspath(root)):
-        #assert type(path) == unicode
-        filesCnt += len(files)
-        dirsCnt  += len(dirs)
-        #bytes    += sum([os.path.getsize(os.path.join(path, s)) for s in files])
-        for s in files:
-            #assert type(s) == unicode
-            file = os.path.join(path, s)
-##            try:
-##                bytes += os.path.getsize(NO_MAXPATH_PFX + file)
-##            except OSError:
-##                sys.stderr.write('Could not access "%s"\n' % file)
-##                sys.exit(1)
-            bytes += getsize(file)
-    return dirsCnt, filesCnt, bytes
-
-
-# A kind of listdir+glob.
-# Accepts *only* DOS wildcards ('?*')
-# and only in the last part of the path (unlike glob).
-# However, it also returns the path spec, like glob.
-# Rules for 'wildcard':
-# - it's always assumed to be a dir
-# - a trailing '\' is ignored (unless the whole string is '\', which means the root)
-def getDirs(wildcard):
-    #assert type(wildcard) == unicode
-
-    # trim w/s and normalize
-    # conv to unicode is a HACK, because normpath(u'.') returns '.'
-    wildcard = unicode(os.path.normpath(wildcard.strip()))
-
-    # remove trailing '\' unless it's the only char in the str
-    if len(wildcard) > 1 and wildcard[-1:] == '\\':
-        wildcard = wildcard[0:-1]
-    
-    path, mask = os.path.split(wildcard)
-
-    # return as-is if there are no wildcards    
-    if not '*' in mask and not '?' in mask:
-        return [wildcard]
-    
-    # build (and use) regex only if needed
-    rx = (doswild2re(mask) if mask and mask <> '*' else None)
-    
-    # HACK for Python bug 818059;
-    # if an empty _unicode_ string is used, listdir() treats it like u'\\'
-    dummyDotAdded = False
-    if not path:
-    	path = type(wildcard)('.')
-    	dummyDotAdded = True
-
-    ret = []
-    for s in os.listdir(path):
-        item = os.path.join(path, s)
-        if os.path.isdir(item) and (not rx or rx.match(s)):
-            ret.append(item)
-
-    # remove dummy '.\'
-    if dummyDotAdded:
-        ret = [s[2:] for s in ret]
-    
-    return ret
-
-
-def doswild2re(s):
-    """Return a case-ignoring regexp from a DOS wildcard."""
-    ret, i, length = type(s)(''), 0, len(s)
-    for c in s:
-        if   c == '*': ret += '.*'
-        elif c == '?': ret += '.'
-        else:          ret += re.escape(c)
-    return re.compile(ret, re.I)
+import wildcard
 
 
 def parse_args():
     UNITS = 'bkmgtpe'
     ap = argparse.ArgumentParser(
-        description='prints the total subdirs/files/bytes of directories',
-        add_help=False)
+        description='prints the total subdirs/files/bytes of directories')
 
     add = ap.add_argument
 
     add('-u', dest='unit', choices=UNITS, default='b',
         help='the outsize size unit; default: %(default)s')
-    add('-d', dest='decs', type=int, default=2,
+    add('-d', dest='decs', type=int, default=0,
         help='output decimal digits (ignored for bytes); default: %(default)s')
-    add('-l', dest='locale', default="",
-        help='locale to use; use an empty string for the system locale; '
-             'note that locale names are case-sensitive and (apart from the "C" locale) '
-             'system-dependant; default: %(default)s')
-    add('-a', dest='acp', action='store_true',
-        help='force output to Windows ANSI codepage')
-    add('dirs', nargs='*', metavar='DIR',
-        help='source directory; wildcards allowed; default is the current dir')
-    add('-?', action='help', help='this help')
+    add('-G', dest='nogrouping', action='store_true',
+        help='disable digit grouping')
+    add('-b', dest='bareoutput', action='store_true',
+        help='bare output (no header)')
+    add('dirs', nargs='+', metavar='DIR',
+        help='source directory path; wildcards allowed (in non-parent parts only)')
 
     args = ap.parse_args()
 
@@ -139,42 +33,76 @@ def parse_args():
     args.unit = 1024 ** UNITS.index(args.unit)
     if args.unit == 1:
         args.decs = 0
-    args.dirs = args.dirs or [u'.']
-    #args.dirs = [unicode(s, 'mbcs') for s in args.dirs]  # FIXME: should use Unicode argv instead
-    
-    try:
-        locale.setlocale(locale.LC_ALL, args.locale)  # FIXME: this seems to aways fail if args.locale != ""
-    except locale.Error as x:
-        ap.error('invalid locale "%s": %s' % (args.locale, x))
 
     return args
 
 
+def deep_dir(root):
+    """Total dirs, files and bytes in the specified dir (recursively).
+
+    Prints errors to STDERR.
+    """
+    def report_error(x):
+        efutil.conerr(str(x))
+    def get_size(path):
+        try:
+            return os.path.getsize(path)
+        except OSError as x:
+            report_error(x)
+            return 0
+    root = os.path.abspath(root)
+    dirs, files, bytes = 0, 0, 0
+    for p,d,f in os.walk(root, onerror=report_error):
+        dirs += len(d)
+        files += len(f)
+        bytes += sum(get_size(os.path.join(p, s)) for s in f)
+    return dirs, files, bytes
+
+
+def match_dirs(pattern):
+    """Generate dir paths from a '[PATH\]WILDCARD' pattern.
+
+    Prints errors to STDERR.
+    """
+    pattern = os.path.normpath(pattern.strip())
+    head, tail = os.path.split(pattern)
+    if not wildcard.iswild(tail):
+        yield pattern
+        return
+    name_rx = wildcard.build(tail or '*')
+    try:
+        names = os.listdir(head or '.')
+    except OSError as x:
+        efutil.conerr(str(x))
+        return
+    for s in names:
+        path = os.path.join(head, s)
+        if os.path.isdir(path) and name_rx.match(s):
+            yield path
+
+
 if __name__ == '__main__':
-
     args = parse_args()
+    try:
+        if not args.bareoutput:
+            print('{:>10} {:>10} {:>15} {}'.format('dirs', 'files', 'size', 'name'))
+            print('{:>10} {:>10} {:>15} {}'.format('----', '-----', '----', '----'))
+        fmtstr = '{:10,} {:10,} {:15,.{}f} {}'
+        if args.nogrouping:
+            fmtstr = fmtstr.replace(',', '')
 
-    print args
-    sys.exit()
+        for pattern in args.dirs:
+            matched_something = False
+            for path in match_dirs(pattern):
+                matched_something = True
+                if not os.path.isdir(path):
+                    efutil.conerr('not a directory: "{}"'.format(path))
+                    continue
+                dirs, files, bytes = deep_dir(path)
+                size = bytes / args.unit
+                efutil.conout(fmtstr.format(dirs, files, size, args.decs, path))
+            if not matched_something:
+                efutil.conerr('nothing matched pattern: "{}"'.format(pattern))
 
-    if args.acp:
-        print_func = lambda s: sys.stdout.write(s.encode('mbcs') + '\n')
-    else:
-        print_func = efutil.uprint
-
-    for mask in args.dirs:
-        # can't use glob() because it skips items starting with '.'
-        # (although the '[]' matching would be nice)
-        for s in getDirs(mask):
-            # just a convenience check for the use;
-            # a race condition can still fool this
-            if not os.path.isdir(s):
-                sys.stderr.write('Invalid directory: "%s"\n' % s)
-                continue
-            t = deepDir(s)
-            # PYTHON BUG: locale.format outputs the wrong width for ints
-            # when grouping is on; this doesn't happen with floats.
-            # That's why t[0]/t[1] are converted to float.
-            print_func(locale.format_string('%10.0f %10.0f %15.*f',
-                (float(t[0]), float(t[1]), args.decs, t[2] / float(args.unit)),
-                True) + ' ' + s)
+    except KeyboardInterrupt:
+        efutil.conout('cancelled')
