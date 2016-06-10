91ecd2306f9a4ddbc37a4d2c470046bdc163d653
elias <none@none>
2016-06-10 09:54:45 +0000

    Added note about occasional failures.

--- thumbs_make.cmd
+++ thumbs_make.cmd
@@ -1,4 +1,7 @@
 :: make thumbnails of all specified items (video files or dirs)
 @echo off & setlocal enableextensions
 set mtn=C:\prg\movie thumbnailer\mtn.exe
+:: NOTE: the min height flag (-h50) causes some thumbs to fail; since the
+:: affected videos are usually short, omitting all of -h -c -r will produce
+:: an acceptable output (unfortunately, this must be done manually)
 "%mtn%" -P -j70 -h50 -c6 -r5 -w1280 -o.jpg %*
