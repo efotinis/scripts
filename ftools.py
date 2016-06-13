62908ea06a9bdc618a4f40f27a338d232dd7d12e
elias <none@none>
2016-06-13 09:41:43 +0000

    Encapsulated video info retrieval in a class.

--- ftools.py
+++ ftools.py
@@ -20,6 +20,11 @@ AVPROBE = 'c:/prg/libav/usr/bin/avprobe.exe'
 
 Image.init()  # maybe preinit() instead?
 
+
+class Error(Exception):
+    pass
+
+
 def files(dp):
     """Get Path file objects of specified dir."""
     return (x for x in dp.iterdir() if x.is_file())
@@ -54,25 +59,6 @@ def move(items, target):
         src.rename(target / src.name)
 
 
-####RX = re.compile(r'^(\d+)\. (.*) \((\d{8})\) \[((?:\w|-)+)\]\.mp4$')
-####def parse_ydl_name(s):
-####    """(index,name,date,id) from 'index. name (date) [id].mp4'."""
-####    return RX.match(s).groups()
-##
-##RX = re.compile(r'^\[(\d{8})\] (.*) \[((?:\w|-)+)\]\.mp4$')
-##def parse_ydl_name(s):
-##    """(date,name,id) from '[date] name [id].mp4'."""
-##    return RX.match(s).groups()
-##
-##if __name__ == '__main__':
-##    (DIR,) = sys.argv[1:]
-##    for s in os.listdir(DIR):
-##        p = os.path.join(DIR, s)
-##        date, name, id_ = parse_ydl_name(s)
-##        duration = fmt_hhmmss(get_duration(p))
-##        print(date, id_, duration, name)
-
-
 def parse_args():
     parser = argparse.ArgumentParser(description='file utilities')
     subs = parser.add_subparsers(dest='cmd')
@@ -89,36 +75,61 @@ def parse_args():
     return parser.parse_args()
 
 
+class VideoInfo:
+    """Video file information."""
+    def __init__(self, path):
+        self.info = avprobe(path)
+
+    def resolution(self):
+        """Get (width,height) of first (and only) video stream."""
+        # check container info
+        try:
+            fmt = self.info['format']
+            return int(fmt['width']), int(fmt['height'])
+        except KeyError:
+            pass
+        # check video stream info
+        vidstr = self._get_single_video_stream()
+        try:
+            return int(vidstr['width']), int(vidstr['height'])
+        except KeyError:
+            raise Error('could not determine resolution')
+
+    def duration(self):
+        """Get duration in seconds."""
+        try:
+            return float(self.info['format']['duration'])
+        except KeyError:
+            raise Error('could not determine duration')
+
+    def bitrate(self):
+        """Get rate in bits per second."""
+        try:
+            return float(self.info['format']['bit_rate'])
+        except KeyError:
+            raise Error('could not determine bitrate')
+
+    def framerate(self):
+        """Get rate in frames per second."""
+        vidstr = self._get_single_video_stream()
+        return int_ratio(vidstr['avg_frame_rate'])
+
+    def _get_single_video_stream(self):
+        vidstrs = [s for s in self.info['streams']
+                   if s['codec_type'] == 'video']
+        if not vidstrs:
+            raise Error('no video streams')
+        if len(vidstrs) > 1:
+            raise Error('more than 1 video streams')
+        return vidstrs[0]
+
+
 VIDEO_EXTS = set('.' + s for s in 'avi wmv mp4 m4v mov mpeg mpg rm flv ogm mkv ram asf webm'.split())
 
 
 def is_video(fp):
     """Test if path has a video extension."""
     return fp.suffix.lower() in VIDEO_EXTS
-
-
-def get_first_stream(info, type_):
-    """Get first video/audio stream or None.
-
-    type_ can be 'video' or 'audio'.
-    A weird WMV had 3 video streams, but the last 2 had an avg_frame_rate=0/0.
-    """
-    a = [stream for stream in info['streams'] if stream['codec_type'] == type_]
-    return a[0] if a else None
-
-
-def get_video_size(info):
-    """Video (width,height).
-
-    Tries the format info first and then the first video stream.
-    """
-    try:
-        return int(info['format']['width']), int(info['format']['height'])
-    except KeyError:
-        vid = get_first_stream(info, 'video')
-        if not vid:
-            raise ValueError('no video streams')
-        return int(vid['width']), int(vid['height'])
 
 
 def int_ratio(s):
@@ -133,27 +144,23 @@ def do_videos(args):
         for f in files(d):
             if is_video(f):
                 try:
-                    info = avprobe(str(f))
-                    duration = float(info['format']['duration'])
-                    bitrate_kbps = round(float(info['format']['bit_rate']) / 1000)
-                    video = get_first_stream(info, 'video')
-                    audio = get_first_stream(info, 'audio')
-                    print(
-                        efutil.timefmt(duration),
-                        '{:4d}x{:<4d}'.format(video['width'], video['height']) if video else '----x----',
-                        '{:5.2f}'.format(int_ratio(video['avg_frame_rate'])) if video else '-----',
-                        '{:4.0f}k'.format(float(video.get('bit_rate', '-1')) / 1000) if video else '----',
-                        f)
-                    durations += [duration]
+                    info = VideoInfo(str(f))
+                    width, height = info.resolution()
+                    print('{} {:4d}x{:<4d} {:5.2f} {:4.0f}k {}'.format(
+                        efutil.timefmt(info.duration()),
+                        width, height,
+                        info.framerate(),
+                        info.bitrate() / 1000,
+                        f))
+                    durations += [info.duration()]
                 except subprocess.CalledProcessError:
                     print('could not get media info: "{}"'.format(str(f)), file=sys.stderr)
     print('total videos:', len(durations))
     print('duration:')
     print('  total:', efutil.timefmt(sum(durations)))
-    print('  average:', efutil.timefmt(stats.amean(durations)))
-    print('  stddev:', efutil.timefmt(stats.stddev(durations)))
+    print('    avg:', efutil.timefmt(stats.amean(durations)))
+    print('     sd:', efutil.timefmt(stats.stddev(durations)))
     
-
 
 def do_images(args):
     if args.outdir and not args.outdir.is_dir():
