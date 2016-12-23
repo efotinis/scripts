b79e84e00888506ffc9523df2dff8af52e9c4644
elias <none@none>
2016-12-23 16:51:25 +0000

    Moved all utilities from user PS profile here.

--- ps_profile.ps1
+++ ps_profile.ps1
@@ -1,14 +1,47 @@
-if ($host.name -eq 'ConsoleHost') {
+# shorthands and aliases
+Function global:x { exit }
+Function global:y { Set-Location D:\youtube }
+function global:z ($time) { c:\scripts\suspend.py -l $time }
+Function global:.. { Set-Location .. }
+Set-Alias -Scope global yd C:\prg\youtube-dl.exe
+Set-Alias -Scope global minf C:\prg\MediaInfo\MediaInfo.exe
 
-    function IsAdmin {
-        ([Security.Principal.WindowsPrincipal] `
-          [Security.Principal.WindowsIdentity]::GetCurrent()
-        ).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+
+# get/set console title
+function global:WndTitle ($s) {
+    if ($s -eq $null) {
+        $Host.UI.RawUI.WindowTitle
+    } else {
+        $Host.UI.RawUI.WindowTitle = $s
     }
-    if (IsAdmin) {
-        $host.UI.RawUI.BackgroundColor = 'darkred'
-        Clear-Host
-    }
+}
+
+
+# play audio file
+function global:PlaySound ($path) {
+    (New-Object Media.SoundPlayer $Path).Play();
+}
+
+
+# reverse input sequence
+# http://stackoverflow.com/questions/22771724/reverse-elements-via-pipeline
+function global:Reversed
+{
+    $a = @($input)
+    [array]::reverse($a)
+    return $a
+}
+
+
+# get elevation status
+function global:IsAdmin {
+    ([Security.Principal.WindowsPrincipal] `
+      [Security.Principal.WindowsIdentity]::GetCurrent()
+    ).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+}
+
+
+if ($host.name -eq 'ConsoleHost') {
 
     # invert prompt text color intensities; helps tell each command apart
     function global:prompt {
@@ -17,6 +50,11 @@ if ($host.name -eq 'ConsoleHost') {
             -ForegroundColor ($host.UI.RawUI.ForegroundColor -bxor 8) `
             -BackgroundColor ($host.UI.RawUI.BackgroundColor -bxor 8)
         Write-Output ' '
+    }
+
+    if (IsAdmin) {
+        $host.UI.RawUI.BackgroundColor = 'darkred'
+        Clear-Host
     }
 
     # customize output stream colors
@@ -28,5 +66,21 @@ if ($host.name -eq 'ConsoleHost') {
     ConClr Warning yellow
     ConClr Debug   cyan
     ConClr Verbose green
+
+    # toggle black & white mode; useful when running programs that output
+    # colors and assuming a black background (e.g. pip)
+    function global:MonoClr {
+        if ([bool]$Env:MonoClr) {
+            if (IsAdmin) {
+                $host.UI.RawUI.BackgroundColor = 'darkred'
+            } else {
+                $host.UI.RawUI.BackgroundColor = 'darkblue'
+            }
+            $Env:MonoClr = $null
+        } else {
+            $host.UI.RawUI.BackgroundColor = 'black'
+            $Env:MonoClr = 'on'
+        }
+    }
 
 }
