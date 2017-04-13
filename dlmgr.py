2724a31c3654ce5334033d5baf35ad1048ab83e5
elias <none@none>
2017-04-13 12:09:24 +0000

    Made more modular and re-usable by other scripts.

--- dlmgr.py
+++ dlmgr.py
@@ -71,18 +71,54 @@ class Status:
         print(msg.ljust(self.lastdisplen), end='\r', flush=True)
         self.lastdisplen = displen
 
-    def end(self, msg):
-        print(msg.ljust(self.lastdisplen))
+    def end(self, msg=''):
+        print(msg.ljust(self.lastdisplen), end='\r', flush=True)
 
 
-def download_with_resume(remote, local, status, blocksize=65536, allow_restart=False, allow_truncate=False):
-    """Download web resource with resume support."""
+class Error(Exception):
+    pass
+
+
+class LocalTooLargeError(Error):
+    def __init__(self, path, localsize, remotesize):
+        self.path = path
+        self.localsize = localsize
+        self.remotesize = remotesize
+    def __str__(self):
+        return 'partial local file is larger than remote ({} > {})'.format(
+            self.localsize , self.remotesize)
+
+
+class Counters:
+    def __init__(self):
+        self.bytes = 0
+        self.seconds = 0
+
+
+def timed_iter_content(resp, size):
+    last_t = time.clock()
+    for data in resp.iter_content(size):
+        t = time.clock()
+        yield data, t - last_t
+        last_t = t
+        
+
+def download_with_resume(remote, local, status, counters=None, blocksize=65536):
+    """Download web resource with resume support.
+
+    Returns True if downloaded successfully (or simply renamed completed partial)
+    or False if already downloaded.
+
+    Special exceptions: LocalTooLargeError.
+
+    If a Counters object is passed, downloaded bytes and elapsed time are
+    accumulated, even on errors.
+    """
 
     if os.path.exists(local):
-        status.end('already downloaded')
-        return True
+        return False  # already downloaded
 
-    TIMEOUT = None
+    TIMEOUT = 10
 
     with contextlib.closing(requests.head(remote, stream=True, timeout=TIMEOUT)) as r:
         canresume = 'bytes' in r.headers.get('Accept-Ranges', '')
@@ -95,31 +131,23 @@ def download_with_resume(remote, local, status, blocksize=65536, allow_restart=F
 
     if localsize is not None and remotesize is not None:
         if localsize == remotesize:
-            status.end('already downloaded')
             os.rename(local_partial, local)
-            return True
+            return True  # already downloaded, but renamed partial
         if localsize > remotesize:
-            if not allow_truncate:
-                status.end('local file is larger than remote')
-                return False
-            with open(local_partial, 'r+b') as f:
-                f.truncate(remotesize)
-            status.end('truncated existing')
-            os.rename(local_partial, local)
-            return True
+            raise LocalTooLargeError(local_partial, localsize, remotesize)
 
-    f = None
+    f = None  # TODO: ctxmgr
     try:
         if canresume and remotesize is not None:
-            #print('resuming range {} ... {}'.format(localsize or 0, remotesize))
+            # resume, known size
             f = open(local_partial, 'w+b' if localsize is None else 'r+b')
             h = {'Range': 'bytes={}-{}'.format(localsize or 0, remotesize)}
         elif canresume and remotesize is None and localsize is not None:
-            #print('resuming range {} ... ?'.format(localsize))
+            # resume, unknown size
             f = open(local_partial, 'r+b')
             h = {'Range': 'bytes={}-'.format(localsize)}
         else:
-            #print('starting download')
+            # start
             f = open(local_partial, 'w+b')
             h = {}
         localsize = localsize or 0
@@ -129,20 +157,19 @@ def download_with_resume(remote, local, status, blocksize=65536, allow_restart=F
             startpos = localsize
             starttime = time.monotonic()
             status.update(startpos, localsize, remotesize, time.monotonic() - starttime)
-            for block in r.iter_content(blocksize):
+            for block, seconds in timed_iter_content(r, blocksize):
                 f.write(block)
                 localsize += len(block)
                 status.update(startpos, localsize, remotesize, time.monotonic() - starttime)
-    except KeyboardInterrupt:    
-        status.end('cancelled')
-        return False
+                counters.bytes += len(block)
+                counters.seconds += seconds
     finally:
+        status.end('')  # TODO: ctxmgr
         if f:
             f.close()
 
-    status.end('done')
     os.rename(local_partial, local)
-    return True
+    return True  # done
 
 
 if __name__ == '__main__':
