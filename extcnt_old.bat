3e9c1263f5bc926fe8adcdb14aea04770aaeaefa
Elias <none@none>
2019-03-31 20:18:11 +0000

    Removed obsolete files.

--- extcnt_old.bat
+++
@@ -1,9 +0,0 @@
-@ECHO OFF & SETLOCAL ENABLEEXTENSIONS & hasHelp.py %* || GOTO skipHelp
-ECHO.Print file extension statistics. [deprecated]
-ECHO.(C) Elias Fotinis
-GOTO :EOF
-:skipHelp
-
-::(FOR /F "delims=" %%A IN ('DIR "%~1" /S /A-D /B') DO @ECHO.%%~xA) | SORT | TXTOPS GRP /U /DF /N /I | SORT /R
-
-DIR "%~1" /S /A-D /B | cscript /nologo "c:\program files\(misc)\file_ext_filter.js" | SORT | TXTOPS GRP /U /DF /N /I | SORT
