1bd585940787771450670882178b00187bbf7ebd
elias <none@none>
2019-07-18 11:30:10 +0000

    Ensure proper output for all archive types.

--- info7.ps1
+++ info7.ps1
@@ -2,6 +2,7 @@
 #
 # Converts 7-Zip's technical listing output to PSCustomObjects.
 
+[CmdletBinding()]
 param(
     [string]$Archive
 )
@@ -32,8 +33,32 @@ function ZipInfo {
         if ($cur -eq $null) {
             if ($_ -eq '----------') {
                 # end of header; start property collection
-                $cur = @{}
+                
+                # NOTE: We initialize with all possible fields, so that the
+                # resulting PSCustomObjects will be consistent; otherwise,
+                # combining the output from disparate archive types will cause
+                # Format-Table to only show columns from first object.
+                
+                # TODO: check other archive types and consolidate all fields
+                $cur = @{
+                    Path=$null
+                    Method=$null
+                    Size=$null
+                    Block=$null
+                    Modified=$null
+                    'Packed Size'=$null
+                    CRC=$null
+                    Attributes=$null
+                    Encrypted=$null
+                }
             }
+            <#elseif ($_ -Match '^Type = (.+)$') {
+                if ($Matches[1] -ne '7z') {
+                    #throw 'not a 7-Zip archive'
+                    Write-Error '*** not a 7zip'
+                    return
+                }
+            }#>
         }
         else {
             if ($_) {
