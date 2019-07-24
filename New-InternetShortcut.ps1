92937c4f83d59a6f567bf263acd3f405ef6cd945
elias <none@none>
2019-07-24 14:23:56 +0000

    Changed method of getting webpage title.

--- New-InternetShortcut.ps1
+++ New-InternetShortcut.ps1
@@ -10,8 +10,14 @@ param(
 Set-StrictMode -Version Latest
 
 if (-not $Title) {
-    $Response = Invoke-WebRequest -Uri $Url
-    $Title = $Response.ParsedHtml.nameProp
+    # NOTE: can't use HtmlWebResponseObject.ParseHtml anymore;
+    # see: https://github.com/PowerShell/PowerShell/issues/5364
+    $Title = py -c @'
+# print webpage title
+import sys, requests, bs4
+url, = sys.argv[1:]
+print(bs4.BeautifulSoup(requests.get(url).content).title.get_text(strip=True), end='')
+'@ $Url
 }
 
 $Path = Join-Path $Destination ((SafeName $Title) + '.url')
