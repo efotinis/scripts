b701e177a941d072938d75c6f48156db5a261079
Elias <none@none>
2019-11-18 17:13:18 +0000

    Ractored and updated for site changes.

--- poseidon.py
+++ poseidon.py
@@ -1,23 +1,17 @@
 #!python3
-#encoding:utf8
+# coding: utf8
 """Create combined weather report images from Poseidon System."""
 
 import argparse
-import collections
 import contextlib
 import datetime
 import io
 import os
-import re
 
 from bs4 import BeautifulSoup
-from dateutil import tz
+import dateutil.tz
 from PIL import Image, ImageDraw, ImageFont
 import requests
-
-
-DAY_OF_WEEK = {i:s for i,s in enumerate('Δευτέρα Τρίτη Τετάρτη Πέμπτη Παρασκευή Σάββατο Κυριακή'.split())}
-MONTH_WITH_DAY_FMT = {i:'{} '+s for i,s in enumerate('Ιανουαρίου Φεβρουαρίου Μαρτίου Απριλίου Μαΐου Ιουνίου Ιουλίου Αυγούστου Σεπτεμβρίου Οκτωβρίου Νοεμβρίου Δεκεμβρίου'.split(), 1)}
 
 
 def parse_args():
@@ -28,120 +22,121 @@ def parse_args():
         help='output directory parent; default is current user desktop')
     args = ap.parse_args()
     if args.outdir is None:
-        args.outdir = os.path.expanduser('~\\Desktop')  # FIXME: proper desktop path
+        # FIXME: use proper API for Desktop path to support non-English locales
+        args.outdir = os.path.expanduser('~\\Desktop')
     return args
 
 
 def utc_to_local(dt):
     """http://stackoverflow.com/questions/4770297/python-convert-utc-datetime-string-to-local-datetime#4771733"""
-    return dt.replace(tzinfo=tz.tzutc()).astimezone(tz.tzlocal())
+    return dt.replace(tzinfo=dateutil.tz.tzutc()).astimezone(dateutil.tz.tzlocal())
 
 
-'''
-16102209 21 10 16gr10 - 2016-10-22 12:00:00+03:00
-http://www.poseidon.hcmr.gr/images/meteo/2016/10/21/gr/grwindb16102209.png
-...
-16102612 21 10 16gr33 - 2016-10-26 15:00:00+03:00
-http://www.poseidon.hcmr.gr/images/meteo/2016/10/21/gr/grwindb16102612.png
-'''
-
-TimePoint = collections.namedtuple('TimePoint', 'timestamp day month year localdate')
-
-
-def img_url(param, tp):
-    """Build image URL."""
-    return 'http://www.poseidon.hcmr.gr/images/meteo/20{}/{}/{}/gr/gr{}{}.png'.format(
-        tp.year, tp.month, tp.day, param, tp.timestamp)
+class LocalizedTitle:
+    WEEK_DAYS = {i:s for i,s in enumerate('Δευτέρα Τρίτη Τετάρτη Πέμπτη Παρασκευή Σάββατο Κυριακή'.split())}
+    DAY_MONTH_FMT = {i:'{} '+s for i,s in enumerate('Ιανουαρίου Φεβρουαρίου Μαρτίου Απριλίου Μαΐου Ιουνίου Ιουλίου Αυγούστου Σεπτεμβρίου Οκτωβρίου Νοεμβρίου Δεκεμβρίου'.split(), 1)}
+    def get(self, dt):
+        return '{}, {}, {:02}:{:02}'.format(
+            self.WEEK_DAYS[dt.weekday()],
+            self.DAY_MONTH_FMT[dt.month].format(dt.day),
+            dt.hour,
+            dt.minute)
 
 
-def get_params(soup):
-    """Generate (id, name) of available weather parameters."""
-    sel = soup.find('select', attrs={'name':'parameterSelect'})
-    for opt in sel.find_all('option'):
-        yield opt['value'], opt.string.strip()
+class WebApp:
+
+    def __init__(self):
+        URL = 'http://poseidonsystem.gr:8004/webapp/weather_forecast/en/?product_id=weather&area_id=gr'
+        self.soup = BeautifulSoup(requests.get(URL).text, 'html.parser')
+        self.model_date = self.soup.find('div', id='model-date').string
+
+    def parameters(self):
+        """List of weather type ID strings."""
+        # the "Select Parameter" combo in the page contains the various
+        # types in the option values as "meteo^...", where "..." is the type
+        options = self.soup.find('select', id='type-picker').find_all('option')
+        for o in options:
+            yield o['value'].partition('^')[2]
+
+    def dates(self):
+        """Prediction dates in pairs of (string id, local datetime).
+        
+        The string ID is a UTC date in '%y%m%d%H' format.
+        """
+        options = self.soup.find('select', id='date-picker').find_all('option')
+        for o in options:
+            id = o['value']
+            dt = datetime.datetime.strptime(id, '%y%m%d%H')
+            yield id, utc_to_local(dt)
+
+    def image_uri(self, param, date):
+        model = self.model_date
+        return f'http://poseidon.hcmr.gr/images/meteo{model}gr/gr{param}{date}.png'
+
+    def multiple_image_data(self, date_id, params):
+        for p in params:
+            url = self.image_uri(p, date_id)
+            with requests.get(url) as r:
+                yield r.content
 
 
-def get_times(soup):
-    """Generate TimePoints of available weather times."""
-    sel = soup.find('select', id='fordaySelect')
-    for opt in sel.find_all('option'):
-        text = opt.string.strip()
-        if text.lower() == 'animation':
-            continue
-        date = utc_to_local(datetime.datetime.strptime(text, '%A, %d-%m-%y Hour %H:%M UTC'))
-        ts, dy, mo, yr = opt['value'].partition('gr')[0].split()
-        yield TimePoint(ts, dy, mo, yr, date)
+class ImageGenerator:
+
+    def __init__(self):
+        self.BANNERH = 80
+        self.IMGW, self.IMGH = 557, 690
+        self.FONT = ImageFont.truetype('arial.ttf', int(self.BANNERH * 0.85))
+        self.clr1, self.clr2 = (240,105,66), (152,185,102)
+        
+    def create(self, outpath, title, images, altclr):
+        images = list(images)
+        im = Image.new('RGB', (self.IMGW * len(images), self.IMGH + self.BANNERH))
+        for i, data in enumerate(images):
+            im2 = Image.open(io.BytesIO(data))
+            im.paste(im2, (self.IMGW * i, self.BANNERH))
+        dr = ImageDraw.Draw(im)
+        clr = self.clr2 if altclr else self.clr1
+        dr.text((0, 0), title, font=self.FONT, fill=clr)
+        im.save(outpath, 'PNG')
 
 
-def greek_time_label(d):
-    return '{}, {}, {:02}:{:02}'.format(
-        DAY_OF_WEEK[d.weekday()],
-        MONTH_WITH_DAY_FMT[d.month].format(d.day),
-        d.hour,
-        d.minute)
+class DateSequenceLargeDelta:
+    def __init__(self, threshold: datetime.timedelta):
+        self.previous = None
+        self.threshold = threshold
+    def check(self, current: datetime.datetime) -> bool:
+        ret = self.previous is not None and current - self.previous > self.threshold
+        self.previous = current
+        return ret
 
 
 def main(args):
-    URL = 'http://www.poseidon.hcmr.gr/weather_forecast.php?area_id=gr'
-    html = requests.get(URL).text
-
-    # remove extraneous closing tag that confuses BeautifulSoup
-    html = re.sub(r'^\s+</option>\n', '', html, count=1, flags=re.MULTILINE)
-
-    soup = BeautifulSoup(html, 'html.parser')
+    desired_params = ['cloud', 'rain', 'windb', 'tem']
+    app = WebApp()
+    assert all(p in app.parameters() for p in desired_params)
 
     outdir = 'poseidon ' + datetime.datetime.now().strftime('%Y-%m-%d %H%M')
     outdir = os.path.join(args.outdir, outdir)
     os.makedirs(outdir, exist_ok=True)
 
-##    timepoints = list(get_times(soup))
-##    for param, name in get_params(soup):
-##        print(name)
-##        for tp in timepoints:
-##            url = img_url(param, tp)
-##            #outpath = os.path.join(outdir, os.path.basename(url))
-##            outstamp = '{0.year:04}-{0.month:02}{0.day:02}-{0.hour:02}{0.minute:02}'.format(tp.localdate)
-##            outpath = os.path.join(outdir, '{} {}.png'.format(param, outstamp))
-##            with contextlib.closing(requests.get(url)) as r, open(outpath, 'wb') as f:
-##                f.write(r.content)
+    imgen = ImageGenerator()
+    loctitle = LocalizedTitle()
+    bigdelta = DateSequenceLargeDelta(datetime.timedelta(hours=3))
+    dates = list(app.dates())
 
-    timepoints = list(get_times(soup))
-    params = list(get_params(soup))
-    wanted = 'cloud rain windb tem'.split()
-    BANNERH = 80
-    IMGW, IMGH = 557, 690
+    for index, (date_id, local_date) in enumerate(dates, 1):
+        print(f'image {index} of {len(dates)} ...', end='\r', flush=True)
 
-    font = ImageFont.truetype('arial.ttf', int(BANNERH * 0.85))
-    
-    prevdate = None
-    for tp in timepoints:
+        is_long_interval = bigdelta.check(local_date)
+        outname = local_date.strftime('%Y-%m%d-%H%M') + '.png'
+        imgen.create(
+            outpath=os.path.join(outdir, outname), 
+            title=loctitle.get(local_date), 
+            images=app.multiple_image_data(date_id, desired_params), 
+            altclr=is_long_interval)
 
-        long_interval = False
-        if prevdate:
-            hours_diff = (tp.localdate - prevdate).total_seconds() / 3600
-            if hours_diff > 3:
-                long_interval = True
-        prevdate = tp.localdate
-
-        outstamp = '{0.year:04}-{0.month:02}{0.day:02}-{0.hour:02}{0.minute:02}'.format(tp.localdate)
-        print(outstamp)
-        images = []
-        for param, name in params:
-            if param not in wanted:
-                continue
-            url = img_url(param, tp)
-            with contextlib.closing(requests.get(url)) as r:
-                images.append(r.content)
-        outpath = os.path.join(outdir, outstamp + '.png')
-        im = Image.new('RGB', (IMGW * len(images), IMGH + BANNERH))
-        for i, data in enumerate(images):
-            im2 = Image.open(io.BytesIO(data))
-            im.paste(im2, (IMGW * i, BANNERH))
-        dr = ImageDraw.Draw(im)
-        clr = (152,185,102) if long_interval else (240,105,66)
-        dr.text((0, 0), greek_time_label(tp.localdate), font=font, fill=clr)
-        im.save(outpath, 'PNG')
+    print(f'generated {len(dates)} images')
 
 
 if __name__ == '__main__':
-    main(parse_args())
+   main(parse_args())
