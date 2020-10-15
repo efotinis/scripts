88749fb0e66f589af38f2585e87eb4f27c3c297d
Elias <none@none>
2020-10-15 08:19:14 +0000

    Changed output format to table.

--- paths.py
+++ paths.py
@@ -9,11 +9,11 @@ import collections
 
 def parse_args():
     ap = argparse.ArgumentParser(
-        description='print PATH directories',
-        epilog='flags shown: "?" non-existing, "!" douplicate')
+        description='print info about PATH directories',
+        epilog='Normal output (i.e. without -b) also show missing and duplicate paths')
     add = ap.add_argument
-    add('-b', dest='bare', action='store_true',
-        help='print only paths')
+    add('-b', dest='bare', action='store_true',  # TODO: remove? too simple
+        help='print only raw paths')
     return ap.parse_args()
 
 
@@ -21,13 +21,22 @@ if __name__ == '__main__':
     args = parse_args()
 
     items = os.environ['PATH'].split(os.path.pathsep)
-    dup_counts = collections.Counter(os.path.normcase(s) for s in items)
     if args.bare:
         for s in items:
             print(s)
     else:
+        dup_counts = collections.Counter(
+            os.path.normcase(os.path.expandvars(s)) for s in items
+        )
+        flag = [ ' ', '*' ]
+        print('mis dup path')
+        print('--- --- ----')
         for s in items:
-            flags = ''
-            flags += '?' if not os.path.isdir(s) else ' '
-            flags += '!' if dup_counts[os.path.normcase(s)] > 1 else ' '
-            print(flags, s)
+            expanded = os.path.expandvars(s)
+            is_missing = not os.path.isdir(expanded)
+            is_duplicate = dup_counts[os.path.normcase(os.path.expandvars(s))] > 1
+            print(' {}   {}  {}'.format(
+                flag[is_missing],
+                flag[is_duplicate],
+                s
+            ))
