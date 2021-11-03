ee5428d961cd72e63917ef96f7653e8a0c40167e
Elias <none@none>
2021-11-03 16:17:27 +0000

    Added delay option.

--- lockdown.py
+++ lockdown.py
@@ -1,6 +1,6 @@
-#!python2
 """Lock the desktop and power down the monitor."""
 
+import argparse
 import time
 import monitorutil
 import dllutil
@@ -9,11 +9,27 @@ from ctypes.wintypes import BOOL
 LockWorkStation = dllutil.winfunc('user32', 'LockWorkStation', BOOL, ())
 
 
+def PositiveFloat(s):
+    n = float(s)
+    return n if n >= 0 else 0
+
+
+def parse_args():
+    ap = argparse.ArgumentParser(
+        description='lock desktop and power down monitor'
+    )
+    ap.add_argument('-d', dest='delay', type=PositiveFloat, default=0.1,
+        help='delay in seconds; may be needed to properly power down monitor; '
+            'default: %(default)s')
+    return ap.parse_args()
+
+
 def lockdown():
     monitorutil.turnoffmonitor()
     LockWorkStation()
 
 
 if __name__ == '__main__':
-    time.sleep(1)
+    args = parse_args()
+    time.sleep(args.delay)
     lockdown()
