fbb5fe40c37e2a7c1e2b650ccebd00c7c9a7920e
Elias Fotinis <efotinis@gmail.com>
2022-09-20 13:08:09 +0300

    Added script from doc scripts.

---
+++ Get-RecycledItem.ps1
@@ -0,0 +1,158 @@
+<#
+.SYNOPSIS
+    Get Recycle Bin item info.
+.DESCRIPTION
+    ...
+.EXAMPLE
+    PS C:\> Get-RecycledItem
+    Name                            Size_ DateDeleted_        OriginalLocation
+    ----                            ----- ------------        ----------------
+    backup - Copy.txt                 127 05/05/2020 21:22:00 C:\Users\Elias\Desktop
+    ps notes - Copy.txt              8100 05/05/2020 21:22:00 C:\Users\Elias\Desktop
+    bathroom faucet leak - Copy.txt    73 05/05/2020 21:22:00 C:\Users\Elias\Desktop
+    bookmarks review - Copy.txt       147 05/05/2020 21:22:00 C:\Users\Elias\Desktop
+    vs code tips - Copy.txt           378 05/05/2020 21:22:00 C:\Users\Elias\Desktop
+    New Text Document - Copy.txt     1864 05/05/2020 21:22:00 C:\Users\Elias\Desktop
+    todotxt - Copy.txt                138 05/05/2020 21:22:00 C:\Users\Elias\Desktop
+.INPUTS
+    --
+.OUTPUTS
+    [PSCustomObject[]], [string[]]
+.NOTES
+    Docs and refs:
+        https://stackoverflow.com/questions/22816215/delete-old-files-in-recycle-bin-with-powershell
+            Delete old files in recycle bin with powershell
+        https://docs.microsoft.com/en-us/windows/win32/api/shlobj_core/nf-shlobj_core-ishelldetails-getdetailsof
+            IShellDetails::GetDetailsOf method
+        https://docs.microsoft.com/en-us/windows/win32/shell/folder-getdetailsof
+            Folder.GetDetailsOf method
+        https://docs.microsoft.com/en-us/windows/desktop/shell/shell-application
+        https://docs.microsoft.com/en-us/windows/desktop/shell/shell-namespace
+        https://docs.microsoft.com/en-us/windows/desktop/shell/folder
+        https://docs.microsoft.com/en-us/windows/desktop/shell/folderitems
+#>
+
+[CmdletBinding()]
+Param(
+    [string[]] $Column = @('Name', 'Size', 'Date deleted', 'Original location'),
+
+    # Handling of date/size strings.
+    [ValidateSet('parsed', 'original', 'both')]
+    [string] $Translatable = 'parsed',
+
+    # Do not Convert column names to simple name indentifiers.
+    [switch] $KeepOriginalNames,
+
+    # List index/name of available data columns, instead of returning any items.
+    [switch] $ListColumns
+)
+
+
+# Column titles of specified folder shell view.
+function GetColumnTitles ($Folder) {
+    for ($i = 0; $s = $Folder.GetDetailsOf($null, $i); $i += 1) {
+        $s
+    }
+}
+
+
+# Convert valid column names to shell view indexes.
+function GetColumnIndexes ($Names, $AllNames) {
+    $h = @{}
+    $i = 0
+    foreach ($c in $AllNames) {
+        $h[$c] = $i
+        ++$i
+    }
+    foreach ($s in $Column) {
+        $index = $h[$s]
+        if ($null -eq $index) {
+            Write-Error "invalid column name: $s"
+        }
+        else {
+            $index
+        }
+        
+    }
+}
+
+
+function ParseSize ($s) {
+    if ($s -notmatch '[\d]+(\.[\d]+)? [bytes|KB|MB|GB|TB]') {
+        0
+    }
+    else {
+        $s = $s -replace ' bytes',''
+        $s = $s -replace ' ',''
+        (Invoke-Expression $s) -as [int64]
+    }
+}
+
+
+function ParseDate ($s) {
+    try {
+        get-date ($s -replace '\u200f|\u200e','')
+    }
+    catch {
+        $null
+    }
+}
+
+
+# Select value parser function based on column name, or $null.
+function GetParser ($name) {
+    switch -Wildcard ($name) {
+        *date* { Get-Item Function:ParseDate }
+        *size* { Get-Item Function:ParseSize }
+    }
+}
+
+
+# Get shell item data.
+function GetData ($Item, $Indexes, $Names, $UseOriginalNames) {
+    $a = [ordered]@{}
+    $Parent = $Item.Parent
+    foreach ($index in $Indexes) {
+        $value = $Parent.GetDetailsOf($Item, $index)
+        $name = $Names[$index]
+        $parser = GetParser $name
+        if (-not $UseOriginalNames) {
+            $name = $name -replace '[^\w]',''
+        }
+        if ($Translatable -ne 'parsed' -or -not $parser) {
+            $a[$name] = $value
+        }
+        if ($Translatable -ne 'original' -and $parser) {
+            $a[$name + '_'] = & $parser $value
+        }
+    }
+    [PSCustomObject]$a
+}
+
+
+$shellObj = New-Object -ComObject Shell.Application
+$recycleBinFolder = $shellObj.NameSpace(0xa)
+$columnNames = GetColumnTitles($recycleBinFolder)
+
+if ($ListColumns) {
+    $columnNames
+    return
+}
+
+$columnIndexes = GetColumnIndexes $Column $columnNames
+
+$progress = @{
+    Activity='Retrieving Recycle Bin items'
+}
+$items = $recycleBinFolder.Items()
+$total = $items.Count
+$count = 0
+foreach ($item in $items) {
+    GetData $item $columnIndexes $columnNames $KeepOriginalNames
+    $count += 1
+    if (($count % 100) -eq 0) {
+        $progress.PercentComplete = $count / $total * 100
+        $progress.Status = "items found: $count"
+        Write-Progress @progress
+    }
+}
