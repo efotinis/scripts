d660754942153e4d4735031076994d7504b138ba
Elias Fotinis <efotinis@gmail.com>
2022-10-04 22:39:52 +0300

    Improved documentation.

--- Get-FilterDirectory.ps1
+++ Get-FilterDirectory.ps1
@@ -3,15 +3,16 @@
     Recursively list directories, skipping items based on name pattern.
 
 .DESCRIPTION
-    Get all subdirectories of the source directories (including themselves), 
+    Get all subdirectories of the source directories (including themselves),
     provided their names do not match any of the wildcard patterns.
-    
-    Can be used to exclude metadata directories (e.g. .git, .vscode) 
-    when searching for actual user files.
+
+    Can be used to exclude meta directories (e.g. .git, .vscode, __pycache__)
+    when searching for actual user files (see examples).
 
 .PARAMETER InputObject
     Source directories to list subdirectories from. They are also included
     in the output, with the same filtering applied to them. Wildcards allowed.
+    Default is the current directory.
 
 .PARAMETER Pattern
     One or more wildcard patterns to match directory names against.
@@ -25,22 +26,34 @@
 
 .EXAMPLE
     PS C:\> Get-FilterDirectory.ps1 C:\Projects\* -Pattern .*
-    
-    Get all dirs and subdirs under C:\Projects as long as they don't have 
+
+    Get all dirs and subdirs under C:\Projects as long as they don't have
     a dot as the initial character.
 
 .EXAMPLE
-    PS C:\> gi C:\script* | Get-FilterDirectory.ps1 -Pattern .*,__pycache__ | ls -file | sls ClassFactoryManagerProxy
-    
-    Search a string in all files of all directories named 'script*' in C:\,
-    excluding directories starting with a dot or named __pycache__.
+    PS C:\> Get-Item C:\script* |
+    >> Get-FilterDirectory.ps1 -Pattern .*,__pycache__ |
+    >> Get-ChildItem -File |
+    >> Select-String ClassFactoryManagerProxy
+
+    This command searches a string in all files of all directories named
+    'script*' in C:\, excluding directories starting with a dot or named
+    __pycache__.
+
+    The pipeline steps are the following:
+        - Get-Item selects the top-level directories.
+        - Get-FilterDirectory gets all subdirs excluding unwanted ones.
+        - Get-ChildItem selects all files.
+            NOTE: Get-ChildItem must specify only File and neither Dir or
+            Recurse for the whole command to work as intended.
+        - Select-String searches the file contents.
 #>
 param(
     [Parameter(Position = 0, ValueFromPipeline, ValueFromPipelineByPropertyName)]
     [Alias('FullName', 'Path')]
     [SupportsWildcards()]
     [string[]]$InputObject = '.',
-    
+
     [Parameter(Mandatory, Position = 1)]
     [string[]]$Pattern
 )
