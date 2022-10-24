4100dcf7476e20e92b9396ebc583a13ad211fdd4
Elias Fotinis <efotinis@gmail.com>
2022-10-24 14:29:37 +0300

    Added absolute path conversion function.

--- ps_profile.ps1
+++ ps_profile.ps1
@@ -779,7 +779,7 @@ function global:tpd {
 .DESCRIPTION
     Gets the reverse of the input string.
 .PARAMETER Text
-    Input string. Can pass multipl values via the pipeline.
+    Input string. Can pass multiple values via the pipeline.
 .INPUTS
     String.
 .OUTPUTS
@@ -801,6 +801,38 @@ function Get-ReverseString {
             [void]$sb.Append($Text[$i])
         }
         Write-Output $sb.ToString()
+    }
+}
+
+
+<#
+.SYNOPSIS
+    Get absolute path.
+.DESCRIPTION
+    Returns absolute path by removing "." and ".." components. Respects
+    PowerShell's current directory. Does not verify that the resulting path
+    exists or is valid.
+.PARAMETER InputObject
+    Input path. Can pass multiple values via the pipeline.
+.INPUTS
+    String.
+.OUTPUTS
+    String.
+.NOTES
+    Source:
+        https://stackoverflow.com/questions/495618/how-to-normalize-a-path-in-powershell
+#>
+function global:Get-AbsolutePath {
+    param(
+        [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)]
+        [AllowEmptyString()]
+        [Alias('Path', 'PSPath')]
+        [string]$InputObject
+    )
+    process {
+        $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath(
+            $InputObject
+        )
     }
 }
 
