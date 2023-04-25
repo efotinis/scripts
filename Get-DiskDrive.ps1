f7095400b2d44cdd6162cf77185e420f4873f050
Elias Fotinis <efotinis@gmail.com>
2023-04-25 20:36:15 +0300

    Added comment help.

--- Get-DiskDrive.ps1
+++ Get-DiskDrive.ps1
@@ -1,3 +1,30 @@
+<#
+.SYNOPSIS
+    Get drive information.
+
+.DESCRIPTION
+    Returns information about existing drives. Returned properties are:
+        - Letter    Assigned letter with colon, e.g. "C:".
+        - Label     Drive label.
+        - Type      Drive type (string). See System.IO.DriveType enum.
+        - Format    Filesystem format.
+        - Ready     Boolean indicating that drive contains media.
+        - Size      Total capacity in bytes.
+        - Free      Total free bytes.
+        - UserFree  Free bytes available to current user (quota).
+        - FreePercentage            Total free perecentage (0-100; float).
+        - UserFreeFreePercentage    User free perecentage (0-100; float).
+
+.PARAMETER All
+    Include drives that are not ready (e.g. optical drives without media). Querying information about such drives introduces a slight delay, so they are excluded by default.
+
+.INPUTS
+    None
+
+.OUTPUTS
+    DiskDrive objects.
+
+#>
 [CmdletBinding()]
 param(
     [switch]$All  # include non-ready
@@ -36,8 +63,8 @@ Add-Type -TypeDefinition @"
 Update-FormatData $Env:SyncMain\util\fmt.ps1xml
 
 
-[System.IO.DriveInfo]::GetDrives() | 
-    Where-Object { $All -or $_.IsReady } | 
+[System.IO.DriveInfo]::GetDrives() |
+    Where-Object { $All -or $_.IsReady } |
     Foreach-Object {
         [DiskDrive]::new(
             $_.Name.Substring(0, 2),
