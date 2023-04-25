0da8d4e6a7a8936c827cd087ace00bcaf3faf548
Elias Fotinis <efotinis@gmail.com>
2023-04-25 20:38:12 +0300

    Changed sizes type to signed to match API.

--- Get-DiskDrive.ps1
+++ Get-DiskDrive.ps1
@@ -33,20 +33,20 @@ param(
 Set-StrictMode -Version Latest
 
 Add-Type -TypeDefinition @"
-    using uint64 = System.UInt64;
+    using int64 = System.Int64;
     public struct DiskDrive {
         public string Letter;
         public string Label;
         public string Type;
         public string Format;
         public bool Ready;
-        public uint64 Size;
-        public uint64 Free;
-        public uint64 UserFree;
+        public int64 Size;
+        public int64 Free;
+        public int64 UserFree;
         public double FreePercentage { get { return Size != 0 ? 100.0*Free/Size : 0; } }
         public double UserFreePercentage { get { return Size != 0 ? 100.0*UserFree/Size : 0; } }
         public DiskDrive(string letter, string label, string type, string format,
-            bool ready, uint64 size, uint64 free, uint64 userFree)
+            bool ready, int64 size, int64 free, int64 userFree)
         {
             Letter = letter;
             Label = label;
