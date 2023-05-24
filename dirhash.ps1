e2fd4d9849e473318b53c50f8d161f1d081333bf
Elias Fotinis <efotinis@gmail.com>
2023-05-24 11:46:12 +0300

    Added rudimentary progress update control. Excessive updating, especially when also writing to stdout, causes considerable slowdown during processing of small files.

--- dirhash.ps1
+++ dirhash.ps1
@@ -3,12 +3,15 @@
 # calculate hashes of all files recursively
 # and also prints relative paths
 
-[CmdLetBinding()]
+[CmdletBinding()]
 param(
   [string[]]$Path = '.',
   [string]$Algorithm = 'sha256',
-  [scriptblock]$Filter = { $true }
+  [scriptblock]$Filter = { $true },
+  [double]$ProgressSeconds
 )
+
+$showProgress = $PSBoundParameters.ContainsKey('ProgressSeconds')
 
 $totalBytes = 0
 $files = foreach ($topDir in $Path) {
@@ -33,10 +36,16 @@ $progress = @{
 }
 $currentFileSize = 0
 
+$progrTimer = [System.Diagnostics.Stopwatch]::new()
+$progrTimer.Start()
+
 filter PreHash {
-    $script:progress.CurrentOperation = PathToRelative $_.FullName
-    $script:progress.Status = "Done: $script:doneFiles of $($files.Count); $(ConvertTo-NiceSize $script:doneBytes) of $(ConvertTo-NiceSize $totalBytes)"
-    Write-Progress @script:progress
+    if ($progrTimer.Elapsed.TotalSeconds -ge $ProgressSeconds) {
+        $script:progress.CurrentOperation = PathToRelative $_.FullName
+        $script:progress.Status = "Done: $script:doneFiles of $($files.Count); $(ConvertTo-NiceSize $script:doneBytes) of $(ConvertTo-NiceSize $totalBytes)"
+        Write-Progress @script:progress
+        $progrTimer.Restart()
+    }
     $script:currentFileSize = $_.Length
     $_
 }
@@ -48,8 +57,10 @@ filter PostHash {
     $_
 }
 
-$files | PreHash | Get-FileHash -Algorithm $Algorithm | PostHash | % { 
+$files | PreHash | Get-FileHash -Algorithm $Algorithm | PostHash | % {
     "$($_.hash) $(PathToRelative $_.path)"
 }
 
-Write-Progress @script:progress -Completed
+if ($showProgress) {
+    Write-Progress @script:progress -Completed
+}
