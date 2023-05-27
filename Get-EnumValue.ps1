fcbd876419807aa110a77ade576c216c07a2c91b
Elias Fotinis <efotinis@gmail.com>
2023-05-27 11:22:42 +0300

    Added comment docs.

--- Get-EnumValue.ps1
+++ Get-EnumValue.ps1
@@ -1,3 +1,25 @@
+<#
+.SYNOPSIS
+    List enum values.
+
+.DESCRIPTION
+    Outputs the names and values of an enumeration.
+
+.PARAMETER Type
+    The enumeration type.
+
+.PARAMETER Hexadecimal
+    Include hexadecimal representation of values in output.
+
+.PARAMETER Binary
+    Include binary representation of values in output.
+
+.INPUTS
+    Type
+
+.OUTPUTS
+    PSCustomObject
+#>
 param(
     [Parameter(Mandatory, ValueFromPipeline)]
     $Type,
