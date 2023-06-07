d8218c50228a9ea92a06c919367f210572cdc2bb
Elias Fotinis <efotinis@gmail.com>
2023-06-07 11:31:50 +0300

    Enabled pipelined input.

--- psmodules/PipelineUtil/PipelineUtil.psm1
+++ psmodules/PipelineUtil/PipelineUtil.psm1
@@ -730,32 +730,42 @@ function Get-ZipArray {
 #>
 function Get-RandomInterleave {
     param(
-        [object[][]]$InputObject
+        [Parameter(Mandatory, Position = 0, ValueFromPipeline)]
+        [object[]]$InputObject
     )
-
-    Set-StrictMode -Version Latest
-
-    $available = [System.Collections.ArrayList]::new()
-
-    $collIndex = 0
-    foreach ($coll in $InputObject) {
-        $collSize = $coll.Count
-        if ($collSize -gt 0) {
-            [void]$available.Add(@{
-                index=$collIndex    # collection index in $InputObject
-                size=$collSize      # size of above collection
-                next=0              # index of next pending item
-            })
-        }
-        $collIndex += 1
+    begin {
+        Set-StrictMode -Version Latest
+        $arrays = [System.Collections.ArrayList]::new()
+        $pipelineMode = -not $PSBoundParameters.ContainsKey('InputObject')
     }
-
-    while ($available) {
-        $i = Get-Random $available.Count
-        $current = $available[$i]
-        $InputObject[$current.index][$current.next]
-        if (++$current.next -ge $current.size) {
-            $available.RemoveAt($i);
+    process {
+        if ($pipelineMode) {
+            [void]$arrays.Add($InputObject)
+        } else {
+            $arrays.AddRange($InputObject)
+        }
+    }
+    end {
+        $available = [System.Collections.ArrayList]::new()
+        $collIndex = 0
+        foreach ($coll in $arrays) {
+            $collSize = $coll.Count
+            if ($collSize -gt 0) {
+                [void]$available.Add(@{
+                    index=$collIndex    # collection index in $arrays
+                    size=$collSize      # size of above collection
+                    next=0              # index of next pending item
+                })
+            }
+            $collIndex += 1
+        }
+        while ($available) {
+            $i = Get-Random $available.Count
+            $current = $available[$i]
+            $arrays[$current.index][$current.next]
+            if (++$current.next -ge $current.size) {
+                $available.RemoveAt($i);
+            }
         }
     }
 }
